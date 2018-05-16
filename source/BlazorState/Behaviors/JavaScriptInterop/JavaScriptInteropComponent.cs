e59c209f183d5d4d9b42600ba2dcb8b4b37dd076
Steven Cramer <Steven.Cramer@LceCorp.com>
2018-05-16 16:46:22 +0000

    Extract JavaScriptInterop Component

---
+++ source/BlazorState/Behaviors/JavaScriptInterop/JavaScriptInteropComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorState.Behaviors.ReduxDevTools
+{
+  using BlazorState.Behaviors.JavaScriptInterop;
+  using Microsoft.AspNetCore.Blazor.Components;
+
+  /// <summary>
+  /// Just sets the Instance for JsonRequestHandler
+  /// </summary>
+  public class JavaScriptInteropComponent : BlazorComponent
+  {
+    [Inject] private JsonRequestHandler JsonRequestHandler { get; set; }
+
+    protected override void OnInit()
+    {
+      JavaScriptInstanceHelper.JsonRequestHandler = JsonRequestHandler;
+    }
+  }
+}
