32c664c6a5da715dace4491a311284dfe026e163
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2018-12-20 06:34:33 +0000

    Move all the template items to approrpiate folders and update projects and slns accordingly.

--- source/TimeWarp.AspNetCore.Blazor.Templates/buildAndInstallTemplate.ps1
+++ source/TimeWarp.AspNetCore.Blazor.Templates/buildAndInstallTemplate.ps1
@@ -1,3 +1,3 @@
-nuget pack .\TimeWarp.AspNetCore.Blazor.Templates.nuspec -Version 0.3.4
+nuget pack .\TimeWarp.AspNetCore.Blazor.Templates.nuspec -Version 0.3.5
 dotnet new -u TimeWarp.AspNetCore.Blazor.Templates
-dotnet new -i TimeWarp.Web.Blazor.Hosted.CSharp.0.3.4.nupkg
+dotnet new -i TimeWarp.AspNetCore.Blazor.Templates.0.3.5.nupkg
