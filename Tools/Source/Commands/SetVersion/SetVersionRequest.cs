3938c82c3a6723e713aba26406d32ec415a62d00
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2019-01-03 05:22:17 +0000

    Add Xml Documentation to request
    This will be used to generate user help.

--- Tools/Source/Commands/SetVersion/SetVersionRequest.cs
+++ Tools/Source/Commands/SetVersion/SetVersionRequest.cs
@@ -2,11 +2,30 @@
 {
   using MediatR;
 
-  internal class SetVersionRequest : IRequest
+  /// <summary>
+  /// Set the version in all required files.
+  /// </summary>
+  public class SetVersionRequest : IRequest
   {
+
+    /// <summary>
+    /// BasePath of source
+    /// </summary>
     public string BasePath { get; set; }
+
+    /// <summary>
+    /// Major Version Number
+    /// </summary>
     public int Major { get; set; }
+
+    /// <summary>
+    /// Minor Version Number
+    /// </summary>
     public int Minor { get; set; }
+
+    /// <summary>
+    /// Patch Version Number
+    /// </summary>
     public int Patch { get; set; }
   }
 }
