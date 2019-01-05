88e4371eff32735e1dbc487814a62c4f5d7547c6
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2019-01-05 11:54:45 +0000

    Test Powershell script.

--- Build/PublishToGitHubPages.ps1
+++ Build/PublishToGitHubPages.ps1
@@ -1,57 +1,57 @@
-[CmdletBinding()]
-param()
+# Display inputs.
+Write-Host "docPath:$docPath" 
+Write-Host "githubusername:$githubusername" 
+Write-Host "githubemail:$githubemail" 
+Write-Host "githubaccesstoken:$githubaccesstoken" 
+Write-Host "repositoryname:$repositoryname" 
+Write-Host "commitMessage:$commitMessage" 
+Write-Host "defaultWorkingDirectory:$defaultWorkingDirectory" 
 
-Trace-VstsEnteringInvocation $MyInvocation
-try {
-    # Get inputs.
-    $docPath = Get-VstsInput -Name 'docPath' -Require
-    $githubusername = Get-VstsInput -Name 'githubusername' -Require
-    $githubemail = Get-VstsInput -Name 'githubemail'
-    $githubaccesstoken = Get-VstsInput -Name 'githubaccesstoken' -Require
-    $repositoryname = Get-VstsInput -Name 'repositoryname' -Require
-    $commitMessage = Get-VstsInput -Name 'commitmessage' -Require
 
-    $defaultWorkingDirectory = Get-VstsTaskVariable -Name 'System.DefaultWorkingDirectory'    
+#$docPath = Get-VstsInput -Name 'docPath' -Require
+#$githubusername = Get-VstsInput -Name 'githubusername' -Require
+#$githubemail = Get-VstsInput -Name 'githubemail'
+#$githubaccesstoken = Get-VstsInput -Name 'githubaccesstoken' -Require
+#$repositoryname = Get-VstsInput -Name 'repositoryname' -Require
+#$commitMessage = Get-VstsInput -Name 'commitmessage' -Require
+
+#$defaultWorkingDirectory = Get-VstsTaskVariable -Name 'System.DefaultWorkingDirectory'    
     
-    Write-Host "Cloning existing GitHub Pages branch"
+#Write-Host "Cloning existing GitHub Pages branch"
 
-    git clone https://${githubusername}:$githubaccesstoken@github.com/$githubusername/$repositoryname.git --branch=gh-pages $defaultWorkingDirectory\ghpages --quiet
+#git clone https://${githubusername}:$githubaccesstoken@github.com/$githubusername/$repositoryname.git --branch=gh-pages $defaultWorkingDirectory\ghpages --quiet
     
-    if ($lastexitcode -gt 0)
-    {
-        Write-Host "##vso[task.logissue type=error;]Unable to clone repository - check username, access token and repository name. Error code $lastexitcode"
-        [Environment]::Exit(1)
-    }
+#if ($lastexitcode -gt 0)
+#{
+#    Write-Host "##vso[task.logissue type=error;]Unable to clone repository - check username, access token and repository name. Error code $lastexitcode"
+#    [Environment]::Exit(1)
+#}
     
-    $to = "$defaultWorkingDirectory\ghpages"
+#$to = "$defaultWorkingDirectory\ghpages"
 
-    Write-Host "Copying new documentation into branch"
+#Write-Host "Copying new documentation into branch"
 
-    Copy-Item $docPath $to -recurse -Force
+#Copy-Item $docPath $to -recurse -Force
 
-    Write-Host "Committing the GitHub Pages Branch"
+#Write-Host "Committing the GitHub Pages Branch"
 
-    cd $defaultWorkingDirectory\ghpages
-    git config core.autocrlf false
-    git config user.email $githubemail
-    git config user.name $githubusername
-    git add *
-    git commit -m $commitMessage
+#cd $defaultWorkingDirectory\ghpages
+#git config core.autocrlf false
+#git config user.email $githubemail
+#git config user.name $githubusername
+#git add *
+#git commit -m $commitMessage
 
-    if ($lastexitcode -gt 0)
-    {
-        Write-Host "##vso[task.logissue type=error;]Error committing - see earlier log, error code $lastexitcode"
-        [Environment]::Exit(1)
-    }
+#if ($lastexitcode -gt 0)
+#{
+#    Write-Host "##vso[task.logissue type=error;]Error committing - see earlier log, error code $lastexitcode"
+#    [Environment]::Exit(1)
+#}
 
-    git push
+#git push
 
-    if ($lastexitcode -gt 0)
-    {
-        Write-Host "##vso[task.logissue type=error;]Error pushing to gh-pages branch, probably an incorrect Personal Access Token, error code $lastexitcode"
-        [Environment]::Exit(1)
-    }
-
-} finally {
-    Trace-VstsLeavingInvocation $MyInvocation
-}
+#if ($lastexitcode -gt 0)
+#{
+#    Write-Host "##vso[task.logissue type=error;]Error pushing to gh-pages branch, probably an incorrect Personal Access Token, error code $lastexitcode"
+#    [Environment]::Exit(1)
+#}
