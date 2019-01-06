fc8e5080b6c77a6db680d29ff981f9eaca053ecd
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2019-01-06 06:07:55 +0000

    email address does not need to be secret

--- Build/PublishToGitHubPages.ps1
+++ Build/PublishToGitHubPages.ps1
@@ -3,15 +3,13 @@ Write-Host "DocPath:$env:DocPath"
 Write-Host "GitHubUsername:$env:GitHubUsername"
 Write-Host "RepositoryName:$env:RepositoryName"
 Write-Host "CommitMessage:$env:CommitMessage" 
-Write-Host "GitHubEmailSecret:$env:GitHubEmailSecret"
+Write-Host "GitHubEmailSecret:$env:GitHubEmail"
 Write-Host "GitHubAccessTokenSecret:$env:GitHubAccessTokenSecret"
 Write-Host "DefaultWorkingDirectory:$env:System_DefaultWorkingDirectory" 
 
-
-    
-Write-Host "Cloning existing GitHub Pages branch"
 $repoUrl = "https://$env:GitHubUsername:`:$env:GitHubAccessTokenSecret" + "@github.com/$env:GitHubUsername/$env:RepositoryName.git"
-Write-Host $repoUrl
+
+Write-Host "Cloning existing GitHub Pages branch"
 
 git clone  $repoUrl --branch=gh-pages $env:System_DefaultWorkingDirectory\ghpages --quiet
     
@@ -20,19 +18,17 @@ if ($lastexitcode -gt 0)
     Write-Host "##vso[task.logissue type=error;]Unable to clone repository - check username, access token and repository name. Error code $lastexitcode"
     [Environment]::Exit(1)
 }
-    
+
 $to = "$env:System_DefaultWorkingDirectory\ghpages"
 
 Write-Host "Copying new documentation into branch"
 
 Copy-Item $env:DocPath $to -recurse -Force
 
-
-
 cd $env:System_DefaultWorkingDirectory\ghpages
 Write-Host "config git"
 git config core.autocrlf false
-git config user.email $env:GitHubEmailSecret
+git config user.email $env:GitHubEmail
 git config user.name $env:GitHubUsername
 git config --list
 
