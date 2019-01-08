383f794caa2f23f34ff24d7ad7a4a34eed86132b
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2019-01-08 08:40:37 +0000

    Generate TocHandler

---
+++ Tools/Source/Commands/GenerateToc/GenerateTocRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tools.Commands.GenerateToc
+{
+  using MediatR;
+
+  /// <summary>
+  /// Generate toc.yml files for all folders found under the documentation folder
+  /// if a toc.yml already exist and does not contain `auto-generated` in the first line this folder will be skipped.
+  /// Generated toc.yml files will have a comment on the first line as in: `# auto-generated`
+  /// </summary>
+  public class GenerateTocRequest : IRequest { }
+}
