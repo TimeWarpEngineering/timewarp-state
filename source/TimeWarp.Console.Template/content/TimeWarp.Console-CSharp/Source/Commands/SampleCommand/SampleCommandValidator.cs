a520dbc86b237b7ddf178afb164ba71eb2dda73c
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2019-01-10 05:39:16 +0000

    Add test to explicitly check the Must in the Validator
    Fix bug in test.

--- source/TimeWarp.Console.Template/content/TimeWarp.Console-CSharp/Source/Commands/SampleCommand/SampleCommandValidator.cs
+++ source/TimeWarp.Console.Template/content/TimeWarp.Console-CSharp/Source/Commands/SampleCommand/SampleCommandValidator.cs
@@ -7,14 +7,14 @@
     public SampleCommandValidator()
     {
       RuleFor(aSampleCommandRequest => aSampleCommandRequest)
-        .Must(ValidateParameter2GreaterthanParameter3)
+        .Must(ValidateParameter3GreaterthanParameter2)
         .WithMessage("Parameter3 must be greater than Parameter2");
       RuleFor(aSetVersionRequest => aSetVersionRequest.Parameter1).MinimumLength(3);
       RuleFor(aSetVersionRequest => aSetVersionRequest.Parameter2).GreaterThanOrEqualTo(0);
       RuleFor(aSetVersionRequest => aSetVersionRequest.Parameter3).LessThanOrEqualTo(10);
     }
 
-    private bool ValidateParameter2GreaterthanParameter3(SampleCommandRequest aSampleCommandRequest) => 
+    private bool ValidateParameter3GreaterthanParameter2(SampleCommandRequest aSampleCommandRequest) => 
       (aSampleCommandRequest.Parameter3 > aSampleCommandRequest.Parameter2);
   }
 }
