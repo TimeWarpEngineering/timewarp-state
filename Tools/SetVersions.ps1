b4f0068cf46e6c3c3ffd5acfe47471ae66abcfd5
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2019-01-17 06:51:34 +0000

    Bump Console Template Version

--- Tools/SetVersions.ps1
+++ Tools/SetVersions.ps1
@@ -1,3 +1,3 @@
 tools SetVersion --Project BlazorState --Major 0 --Minor 3 --Patch 6
 tools SetVersion --Project BlazorTemplate --Major 0 --Minor 3 --Patch 6
-tools SetVersion --Project ConsoleTemplate --Major 0 --Minor 0 --Patch 2
+tools SetVersion --Project ConsoleTemplate --Major 0 --Minor 0 --Patch 3
