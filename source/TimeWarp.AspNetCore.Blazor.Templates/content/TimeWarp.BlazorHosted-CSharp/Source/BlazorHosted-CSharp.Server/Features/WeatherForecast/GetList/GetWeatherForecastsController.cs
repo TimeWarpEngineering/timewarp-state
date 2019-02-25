a87f070f84b34753f828a4d73eebbe34f9dcd05a
StevenTCramer <SteveTCramer@gmail.com>
2019-02-25 04:32:39 +0000

    Move Mediator to property injection
    This simplfies the controllers in that they no longer need to overide the constructor for every instance.

--- source/TimeWarp.AspNetCore.Blazor.Templates/content/TimeWarp.BlazorHosted-CSharp/Source/BlazorHosted-CSharp.Server/Features/WeatherForecast/GetList/GetWeatherForecastsController.cs
+++ source/TimeWarp.AspNetCore.Blazor.Templates/content/TimeWarp.BlazorHosted-CSharp/Source/BlazorHosted-CSharp.Server/Features/WeatherForecast/GetList/GetWeatherForecastsController.cs
@@ -3,14 +3,11 @@
   using System.Threading.Tasks;
   using BlazorHosted_CSharp.Server.Features.Base;
   using BlazorHosted_CSharp.Shared.Features.WeatherForecast;
-  using MediatR;
   using Microsoft.AspNetCore.Mvc;
 
   [Route(GetWeatherForecastsRequest.Route)]
   public class GetWeatherForecastsController : BaseController<GetWeatherForecastsRequest, GetWeatherForecastsResponse>
   {
-    public GetWeatherForecastsController(IMediator aMediator) : base(aMediator) { }
-
     public async Task<IActionResult> Get(GetWeatherForecastsRequest aRequest) => await Send(aRequest);
   }
 }
