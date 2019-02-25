90c2109b5fd58dcc9ac155ac438d0b9511572b45
StevenTCramer <SteveTCramer@gmail.com>
2019-02-25 02:11:58 +0000

    Update docs and comment

--- source/TimeWarp.AspNetCore.Blazor.Templates/content/TimeWarp.BlazorHosted-CSharp/Source/BlazorHosted-CSharp.Client/Components/BaseComponent.cs
+++ source/TimeWarp.AspNetCore.Blazor.Templates/content/TimeWarp.BlazorHosted-CSharp/Source/BlazorHosted-CSharp.Client/Components/BaseComponent.cs
@@ -12,7 +12,7 @@
   /// <remarks>
   /// In production one would NOT be required to use these base components
   /// But would be required to properly implement the required interfaces.
-  /// one could conditionally inherit from BlazorComponent for production build.
+  /// one could conditionally inherit from BaseComponent for production build.
   /// </remarks>
   public class BaseComponent : BlazorStateDevToolsComponent
   {
