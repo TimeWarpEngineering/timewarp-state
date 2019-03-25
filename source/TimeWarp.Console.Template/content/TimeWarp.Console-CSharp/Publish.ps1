c81f089cbe71725b80c561210cb50fb611c02bb4
Steven T. Cramer <SteveTCramer@gmail.com>
2019-03-25 11:49:32 +0000

    Remove console template
    It is now part of the timewarp-templates repo.

--- source/TimeWarp.Console.Template/content/TimeWarp.Console-CSharp/Publish.ps1
+++
@@ -1,8 +0,0 @@
-# TODO build for other environments
-dotnet publish .\Source\Console-CSharp.csproj --configuration Release --runtime win10-x64
-
-
-$CommandDir = $PSScriptRoot + "\bin\netcoreapp2.2\win10-x64"
-if (!$env:Path.Contains($CommandDir)) { 
-  $env:Path += ";" + $CommandDir
-}
