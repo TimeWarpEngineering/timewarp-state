1922b1f90b10a37760a973f692603dfa09a752bd
Steven T. Cramer <SteveTCramer@gmail.com>
2019-03-26 14:20:08 +0000

    Fix potential null reference error
    Rename BlazorState.Unit.Tests to BlazorState.Tests

--- source/BlazorState/InternalsVisibleToTest.cs
+++ source/BlazorState/InternalsVisibleToTest.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("BlazorState.Client.Integration.Tests")]
-[assembly: InternalsVisibleTo("BlazorState.Unit.Tests")]
+[assembly: InternalsVisibleTo("BlazorState.Tests")]
