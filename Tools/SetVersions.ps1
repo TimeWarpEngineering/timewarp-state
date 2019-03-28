b87b2dfe68711a4b9e446308481924c60c8534ef
Steven T. Cramer <SteveTCramer@gmail.com>
2019-03-28 05:55:36 +0000

    Bump version

--- Tools/SetVersions.ps1
+++ Tools/SetVersions.ps1
@@ -1,2 +1,2 @@
-tools SetVersion --Project BlazorState --Major 0 --Minor 4 --Patch 2
-tools SetVersion --Project BlazorTemplate --Major 0 --Minor 4 --Patch 2
+tools SetVersion --Project BlazorState --Major 0 --Minor 4 --Patch 3
+tools SetVersion --Project BlazorTemplate --Major 0 --Minor 4 --Patch 3
