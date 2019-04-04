3ea4a3cb9dc8c9aceeee96453bc76c4dde25c9e1
Steven T. Cramer <SteveTCramer@gmail.com>
2019-04-04 04:20:22 +0000

    Add Reset Button to Test App

---
+++ test/TestApp/TestApp.Client/Components/ResetButton.cshtml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApp.Client.Components
+{
+  using TestApp.Client.Features.Application;
+
+  public class ResetButtonModel : BaseComponent
+  {
+    internal void ButtonClick() => Mediator.Send(new ResetStoreAction());
+  }
+}
---
+++ test/TestApp/TestApp.Client/Features/Application/Actions/ResetStoreHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace TestApp.Client.Features.Application
+{
+  using System.Threading;
+  using System.Threading.Tasks;
+  using BlazorState;
+  using BlazorState.Features.Routing;
+  using MediatR;
+
+  public class ResetStoreHandler : IRequestHandler<ResetStoreAction>
+  {
+    public ResetStoreHandler(IStore aStore, IMediator aMediator)
+    {
+      Mediator = aMediator;
+      Store = aStore;
+    }
+
+    private IMediator Mediator { get; }
+    private IStore Store { get; }
+
+    public Task<Unit> Handle(ResetStoreAction aResetStoreAction, CancellationToken aCancellationToken)
+    {
+      Store.Reset();
+      Mediator.Send(new ChangeRouteRequest
+      {
+        NewRoute = "/"
+      });
+      return Unit.Task;
+    }
+  }
+}
