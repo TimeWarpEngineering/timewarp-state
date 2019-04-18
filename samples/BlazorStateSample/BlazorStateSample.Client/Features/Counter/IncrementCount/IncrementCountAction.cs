d30ee610af5683970e9ab08c21db127de2698ffa
Steven T. Cramer <SteveTCramer@gmail.com>
2019-04-18 05:28:29 +0000

    Update to latest nightly sdk and preview4
    Remove Samples

--- samples/BlazorStateSample/BlazorStateSample.Client/Features/Counter/IncrementCount/IncrementCountAction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorStateSample.Client.Features.Counter.IncrementCount
-{
-  using MediatR;
-
-  public class IncrementCountAction : IRequest<CounterState>
-  {
-    public int Amount { get; set; }
-  }
-}
