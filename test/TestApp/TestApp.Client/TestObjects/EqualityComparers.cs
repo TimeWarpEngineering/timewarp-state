9d759489e19d6304c27794d73d03fa1a5b91bde3
Steven T. Cramer <SteveTCramer@gmail.com>
2019-05-08 18:28:03 +0000

    Coding Conventions
    minor cleanup

--- test/TestApp/TestApp.Client/TestObjects/EqualityComparers.cs
+++ test/TestApp/TestApp.Client/TestObjects/EqualityComparers.cs
@@ -1,33 +1,31 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace AnyClone.Tests.TestObjects
+﻿namespace AnyClone.Tests.TestObjects
 {
-    public class DictionaryComparer<TKey, TValue> : IEqualityComparer<IDictionary<TKey, TValue>>
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public class DictionaryComparer<TKey, TValue> : IEqualityComparer<IDictionary<TKey, TValue>>
+  {
+    private IEqualityComparer<TValue> valueComparer { get; }
+
+    public DictionaryComparer(IEqualityComparer<TValue> aValueComparer = null)
     {
-        private IEqualityComparer<TValue> valueComparer;
-        public DictionaryComparer(IEqualityComparer<TValue> valueComparer = null)
-        {
-            this.valueComparer = valueComparer ?? EqualityComparer<TValue>.Default;
-        }
-
-        public bool Equals(IDictionary<TKey, TValue> x, IDictionary<TKey, TValue> y)
-        {
-            if (x.Count != y.Count)
-                return false;
-            if (x.Keys.Except(y.Keys).Any())
-                return false;
-            if (y.Keys.Except(x.Keys).Any())
-                return false;
-            foreach (var pair in x)
-                if (!valueComparer.Equals(pair.Value, y[pair.Key]))
-                    return false;
-            return true;
-        }
-
-        public int GetHashCode(IDictionary<TKey, TValue> obj)
-        {
-            return 0;
-        }
+      valueComparer = aValueComparer ?? EqualityComparer<TValue>.Default;
     }
+
+    public bool Equals(IDictionary<TKey, TValue> aDictionary1, IDictionary<TKey, TValue> aDictionary2)
+    {
+      if (aDictionary1.Count != aDictionary2.Count) return false;
+      if (aDictionary1.Keys.Except(aDictionary2.Keys).Any()) return false;
+      if (aDictionary2.Keys.Except(aDictionary1.Keys).Any()) return false;
+
+      foreach (KeyValuePair<TKey, TValue> pair in aDictionary1)
+      {
+        if (!valueComparer.Equals(pair.Value, aDictionary2[pair.Key])) return false;
+      }
+
+      return true;
+    }
+
+    public int GetHashCode(IDictionary<TKey, TValue> aDictionary) => 0;
+  }
 }
