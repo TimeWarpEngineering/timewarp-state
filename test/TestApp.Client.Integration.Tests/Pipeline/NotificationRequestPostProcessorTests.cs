72895546f9ac6cfbde0ff215c1abc13a1beb0006
Steven T. Cramer <SteveTCramer@gmail.com>
2019-05-08 18:38:15 +0000

    Clean up placeholder

--- test/TestApp.Client.Integration.Tests/Pipeline/NotificationRequestPostProcessorTests.cs
+++ test/TestApp.Client.Integration.Tests/Pipeline/NotificationRequestPostProcessorTests.cs
@@ -1,19 +1,12 @@
 ﻿namespace TestApp.Client.Integration.Tests.Pipeline
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Text;
-  using System.Threading.Tasks;
   using TestApp.Client.Integration.Tests.Infrastructure;
 
   internal class NotificationRequestPostProcessorTests
   {
     public NotificationRequestPostProcessorTests(TestFixture aTestFixture)
     {
-
+      // TODO
     }
-
-
   }
 }
