eec38e8d4d4ee95be4da02fed2d604e565c0d728
Steven T. Cramer <SteveTCramer@gmail.com>
2019-06-04 02:42:19 +0000

    Make struct readonly and autogenerate methods
    I don't think this is needed for my use case but it is rather easy to do.

--- source/BlazorState/Subscriptions.cs
+++ source/BlazorState/Subscriptions.cs
@@ -7,11 +7,33 @@
 
   public class Subscriptions
   {
-    struct Subscription
+    private readonly struct Subscription
     {
-      public Type StateType { get; set; }
-      public string ComponentId { get; set; }
-      public WeakReference<BlazorStateComponent> BlazorStateComponentReference { get; set; }
+      public Subscription(Type aStateType, string aComponentId, WeakReference<BlazorStateComponent> aBlazorStateComponentReference)
+      {
+        StateType = aStateType;
+        ComponentId = aComponentId;
+        BlazorStateComponentReference = aBlazorStateComponentReference;
+      }
+
+      public Type StateType { get; }
+      public string ComponentId { get; }
+      public WeakReference<BlazorStateComponent> BlazorStateComponentReference { get;}
+
+      public override bool Equals(object aObject) => 
+        aObject is Subscription subscription && 
+        EqualityComparer<Type>.Default.Equals(StateType, subscription.StateType) && 
+        ComponentId == subscription.ComponentId && 
+        EqualityComparer<WeakReference<BlazorStateComponent>>.Default.Equals(BlazorStateComponentReference, subscription.BlazorStateComponentReference);
+
+      public override int GetHashCode()
+      {
+        int hashCode = -1827213943;
+        hashCode = hashCode * -1521134295 + EqualityComparer<Type>.Default.GetHashCode(StateType);
+        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ComponentId);
+        hashCode = hashCode * -1521134295 + EqualityComparer<WeakReference<BlazorStateComponent>>.Default.GetHashCode(BlazorStateComponentReference);
+        return hashCode;
+      }
     }
 
     public Subscriptions(ILogger<Subscriptions> aLogger)
@@ -34,12 +56,11 @@
       // Add only once.
       if (!BlazorStateComponentReferencesList.Any(aSubscription => aSubscription.StateType == aType && aSubscription.ComponentId == aBlazorStateComponent.Id))
       {
-        var subscription = new Subscription
-        {
-          ComponentId = aBlazorStateComponent.Id,
-          StateType = aType,
-          BlazorStateComponentReference = new WeakReference<BlazorStateComponent>(aBlazorStateComponent)
-        };
+        var subscription = new Subscription(
+          aType,
+          aBlazorStateComponent.Id,
+          new WeakReference<BlazorStateComponent>(aBlazorStateComponent));
+
         BlazorStateComponentReferencesList.Add(subscription);
       }
 
@@ -87,6 +108,16 @@
           BlazorStateComponentReferencesList.Remove(subscription);
         }
       }
+    }
+
+    public override bool Equals(object obj) => obj is Subscriptions subscriptions && EqualityComparer<ILogger>.Default.Equals(Logger, subscriptions.Logger) && EqualityComparer<List<Subscription>>.Default.Equals(BlazorStateComponentReferencesList, subscriptions.BlazorStateComponentReferencesList);
+
+    public override int GetHashCode()
+    {
+      var hashCode = -914156548;
+      hashCode = hashCode * -1521134295 + EqualityComparer<ILogger>.Default.GetHashCode(Logger);
+      hashCode = hashCode * -1521134295 + EqualityComparer<List<Subscription>>.Default.GetHashCode(BlazorStateComponentReferencesList);
+      return hashCode;
     }
   }
 }
