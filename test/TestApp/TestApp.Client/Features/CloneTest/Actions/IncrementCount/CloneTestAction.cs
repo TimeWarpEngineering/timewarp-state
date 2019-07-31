da780156141da1135da962a7c5b94f8ecb373ae9
Steven T. Cramer <SteveTCramer@gmail.com>
2019-07-31 07:22:27 +0000

    Add test to duplicate the clone is not calling the overrident clone on the state.

---
+++ test/TestApp/TestApp.Client/Features/CloneTest/Actions/IncrementCount/CloneTestAction.cs
@@ -0,0 +1,7 @@
+﻿namespace TestApp.Client.Features.CloneTest
+{
+  using MediatR;
+  using TestApp.Shared.Features.Base;
+
+  public class CloneTestAction : BaseRequest, IRequest<CloneTestState>  {  }
+}
