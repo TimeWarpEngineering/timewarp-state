fe7dc5542db9877e8c6a00a23a8d0aa180cb379c
Steven T. Cramer <Steve.Cramer@TimeWarp.Enterprises>
2019-08-17 08:40:36 +0000

    the Test Fixture wasn't using the TestServer.
    It was trying to configure everything in the constructor but should have just used th TestServer

--- test/TestApp.Client.Integration.Tests/Infrastructure/BlazorStateTestServer.cs
+++ test/TestApp.Client.Integration.Tests/Infrastructure/BlazorStateTestServer.cs
@@ -4,6 +4,11 @@
   using Microsoft.AspNetCore.Hosting;
   using Microsoft.AspNetCore.TestHost;
 
+
+  /// <summary>
+  /// This is a test version of the `TestApp.Server`.
+  /// The `TestFixture` class will use the HttpClient from this class
+  /// </summary>
   public class BlazorStateTestServer : TestServer
   {
     public BlazorStateTestServer() : base(WebHostBuilder()) { }
--- test/TestApp.Server.Integration.Tests/Infrastructure/TestFixture.cs
+++ test/TestApp.Server.Integration.Tests/Infrastructure/TestFixture.cs
@@ -1,31 +1,19 @@
-﻿namespace TestApp.Server.Integration.Tests
+﻿namespace TestApp.Server.Integration.Tests.Infrastructure
 {
   using System;
-  using System.IO;
-  using Microsoft.Extensions.Configuration;
-  using Microsoft.Extensions.DependencyInjection;
 
   public class TestFixture
   {
-    public TestFixture()
-    {
-      IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile(path: "appsettings.json", optional: true, reloadOnChange: true)
-        .AddEnvironmentVariables();
-      ConfigurationRoot = configurationBuilder.Build();
-
-      var startup = new Startup();
-      var services = new ServiceCollection();
-      startup.ConfigureServices(services);
-      ServiceProvider = services.BuildServiceProvider();
-    }
+    private readonly BlazorStateTestServer BlazorStateTestServer;
 
     /// <summary>
     /// This is the ServiceProvider that will be used by the Server
     /// </summary>
-    public IServiceProvider ServiceProvider { get; set; }
+    public IServiceProvider ServiceProvider => BlazorStateTestServer.Services;
 
-    public IConfigurationRoot ConfigurationRoot { get; }
+    public TestFixture(BlazorStateTestServer aBlazorStateTestServer)
+    {
+      BlazorStateTestServer = aBlazorStateTestServer;
+    }
   }
 }
