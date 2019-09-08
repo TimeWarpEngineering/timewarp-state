f854046f7681bd146431127adca6dd2bf56b7720
Steven T. Cramer <Steve.Cramer@TimeWarp.Enterprises>
2019-09-08 16:39:38 +0000

    Add End-to-end Test case for ChangeRoute

---
+++ Tests/TestApp/Client/Pages/CounterPage.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace TestApp.Client.Pages
+{
+  using BlazorState.Features.Routing;
+  using System.Threading.Tasks;
+  using TestApp.Client.Features.Base.Components;
+
+  public class CounterPageBase : BaseComponent
+  {
+    protected async Task ButtonClick() =>
+      _ = await Mediator.Send(new RouteState.ChangeRouteAction { NewRoute = "/" });
+  }
+}
