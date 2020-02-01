4117ffc148d0e6dbdb5e6caca459a83360998212
StevenTCramer <Steven.Cramer@CineMassive.com>
2020-02-01 09:32:44 +0000

    formatting and documentation only

--- Tests/TestApp/Client/Features/Base/BaseHandler.cs
+++ Tests/TestApp/Client/Features/Base/BaseHandler.cs
@@ -9,16 +9,13 @@
   /// <summary>
   /// Base Handler that makes it easy to access state
   /// </summary>
-  /// <typeparam name="TAction"></typeparam>
+  /// <typeparam name="TAction">The Type of Action to be handled</typeparam>
   internal abstract class BaseHandler<TAction> : ActionHandler<TAction>
     where TAction : IAction
   {
     protected ApplicationState ApplicationState => Store.GetState<ApplicationState>();
-
     protected CounterState CounterState => Store.GetState<CounterState>();
-
     protected EventStreamState EventStreamState => Store.GetState<EventStreamState>();
-
     protected WeatherForecastsState WeatherForecastsState => Store.GetState<WeatherForecastsState>();
 
     public BaseHandler(IStore aStore) : base(aStore) { }
