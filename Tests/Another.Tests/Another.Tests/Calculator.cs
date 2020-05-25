4d7db8c6fa3acef799ffcb749dd75582f825f0d0
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-05-25 12:51:26 +0000

    Adding another test project to see why the TestConvention isn't working this will be thrown away.

---
+++ Tests/Another.Tests/Another.Tests/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace Another.Tests
+{
+  internal class Calculator
+  {
+    internal int Add(int v1, int v2) => v1 + v2;
+
+    internal object Subtract(int v1, int v2) => v1 - v2;
+  }
+}
---
+++ Tests/Another.Tests/Another.Tests/Infrastructure/TestingConvention.cs
@@ -0,0 +1,54 @@
+﻿namespace Another.Tests.Infrastructure
+{
+  using Fixie;
+  using Microsoft.Extensions.DependencyInjection;
+  public class TestingConvention : Discovery, Execution
+  {
+    const string TestPostfix = "_Tests";
+
+    private readonly IServiceScopeFactory ServiceScopeFactory;
+    public TestingConvention()
+    {
+      var testServices = new ServiceCollection();
+      ConfigureTestServices(testServices);
+      ServiceProvider serviceProvider = testServices.BuildServiceProvider();
+      ServiceScopeFactory = serviceProvider.GetService<IServiceScopeFactory>();
+
+      Classes.Where(aType => aType.Namespace.EndsWith(TestPostfix));
+      Methods.Where(aMethodExpression => aMethodExpression.Name != nameof(Setup));
+    }
+
+    public void Execute(TestClass aTestClass)
+    {
+      aTestClass.RunCases
+      (
+        aCase =>
+        {
+          using IServiceScope serviceScope = ServiceScopeFactory.CreateScope();
+          object instance = serviceScope.ServiceProvider.GetService(aTestClass.Type);
+          Setup(instance);
+
+          aCase.Execute(instance);
+        }
+      );
+    }
+
+    private static void Setup(object aInstance)
+    {
+      System.Reflection.MethodInfo method = aInstance.GetType().GetMethod(nameof(Setup));
+      method?.Execute(aInstance);
+    }
+
+    private void ConfigureTestServices(ServiceCollection aServiceCollection)
+    {
+      aServiceCollection.Scan
+      (
+        aTypeSourceSelector => aTypeSourceSelector
+          .FromAssemblyOf<TestingConvention>()
+          .AddClasses(action: (aClasses) => aClasses.Where(aClass => aClass.Namespace.EndsWith(TestPostfix)))
+          .AsSelf()
+          .WithScopedLifetime()
+      );
+    }
+  }
+}
