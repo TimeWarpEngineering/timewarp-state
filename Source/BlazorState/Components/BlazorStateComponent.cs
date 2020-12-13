d1b784d40daa01794ce7d76668c02d4b2b8b0391
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-12-13 04:25:13 +0000

    Make Dispose virtual so others can override.

--- Source/BlazorState/Components/BlazorStateComponent.cs
+++ Source/BlazorState/Components/BlazorStateComponent.cs
@@ -60,6 +60,6 @@ namespace BlazorState
       return Store.GetState<T>();
     }
 
-    public void Dispose() => Subscriptions.Remove(this);
+    public virtual void Dispose() => Subscriptions.Remove(this);
   }
 }
