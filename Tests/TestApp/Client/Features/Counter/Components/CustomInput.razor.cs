abd16acd52a8fa9cfe22fe1cc1d929bd57a83388
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-08-15 16:18:34 +0000

    Cleaning up the CustomInput Example

--- Tests/TestApp/Client/Features/Counter/Components/CustomInput.razor.cs
+++ Tests/TestApp/Client/Features/Counter/Components/CustomInput.razor.cs
@@ -6,58 +6,49 @@
   using System.Threading.Tasks;
   using Microsoft.AspNetCore.Components;
   using TestApp.Client.Features.Base.Components;
-  using static TestApp.Client.Features.Counter.CounterState;
 
-  public class CustomInputBase<T>: BaseInputComponent<T>
+  public partial class CustomInput<T>: BaseInputComponent<T>
   {
-    [Parameter] public string Id { get; set; }
     [Parameter] public string Label { get; set; }
     [Parameter] public Expression<Func<T>> ValidationFor { get; set; }
 
     protected override bool TryParseValueFromString(string aValue, out T aResult, out string aValidationErrorMessage)
     {
+      bool canParse;
+      aValidationErrorMessage = null;
+      aResult = default;
+
       if (typeof(T) == typeof(string))
       {
         aResult = (T)(object)aValue;
-        aValidationErrorMessage = null;
-
-        return true;
+        canParse = true;
       }
       else if (typeof(T) == typeof(int))
       {
-        int.TryParse(aValue, NumberStyles.Integer, CultureInfo.InvariantCulture, out var parsedValue);
-        aResult = (T)(object)parsedValue;
-        aValidationErrorMessage = null;
-
-        return true;
+        canParse = int.TryParse(aValue, NumberStyles.Integer, CultureInfo.InvariantCulture, out int parsedValue);
+        if (canParse) aResult = (T)(object)parsedValue;
       }
       else if (typeof(T) == typeof(Guid))
       {
-        Guid.TryParse(aValue, out Guid parsedValue);
-        aResult = (T)(object)parsedValue;
-        aValidationErrorMessage = null;
-
-        return true;
+        canParse = Guid.TryParse(aValue, out Guid parsedValue);
+        if (canParse) aResult = (T)(object)parsedValue; ;
       }
       else if (typeof(T).IsEnum)
       {
-        try
-        {
-          aResult = (T)Enum.Parse(typeof(T), aValue);
-          aValidationErrorMessage = null;
-
-          return true;
-        }
-        catch (ArgumentException)
-        {
-          aResult = default;
-          aValidationErrorMessage = $"The {FieldIdentifier.FieldName} field is not valid.";
-
-          return false;
-        }
+        canParse = Enum.TryParse(typeof(T), aValue, out object parsedValue);
+        if (canParse) aResult = (T)(object)parsedValue;
+      }
+      else
+      {
+        throw new InvalidOperationException($"{GetType()} does not support the type '{typeof(T)}'.");
       }
 
-      throw new InvalidOperationException($"{GetType()} does not support the type '{typeof(T)}'.");
+      if (!canParse)
+      {
+        aValidationErrorMessage = $"The {FieldIdentifier.FieldName} field is not valid.";
+      }
+
+      return canParse;
     }
 
     protected async Task ButtonClick() => 
