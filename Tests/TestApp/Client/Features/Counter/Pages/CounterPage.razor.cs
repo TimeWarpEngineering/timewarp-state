8e0dac7c4d9032d70f61e11973ca45306faf393e
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-08-15 12:21:44 +0000

    fix the merge

--- Tests/TestApp/Client/Features/Counter/Pages/CounterPage.razor.cs
+++ Tests/TestApp/Client/Features/Counter/Pages/CounterPage.razor.cs
@@ -1,8 +1,10 @@
 namespace TestApp.Client.Pages
 {
   using BlazorState.Features.Routing;
+  using System;
   using System.Threading.Tasks;
   using TestApp.Client.Features.Base.Components;
+  using TestApp.Client.Models;
   using static TestApp.Client.Features.Counter.CounterState;
 
   public partial class CounterPage : BaseComponent
@@ -15,6 +17,12 @@ namespace TestApp.Client.Pages
 
     protected async Task SendThrowServerSideExceptionAction() =>
       await Mediator.Send(new ThrowServerSideExceptionAction());
+
+    public Record RecordForm { get; set; } = new Record();
+    protected void HandleValidSubmit()
+    {
+      Console.WriteLine("Valid Submit");
+    }
 
   }
 }
