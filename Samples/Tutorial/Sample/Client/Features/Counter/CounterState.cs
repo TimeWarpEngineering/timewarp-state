5af4b83bb4143293adff1deeac04326e99c3d785
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-10-17 14:48:11 +0000

    Add Counter State

---
+++ Samples/Tutorial/Sample/Client/Features/Counter/CounterState.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample.Client.Features.Counter
+{
+  using BlazorState;
+
+  public partial class CounterState : State<CounterState>
+  {
+    public int Count { get; private set; }
+    public override void Initialize() => Count = 3;
+  }
+}
