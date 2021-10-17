0bb163ff3cb025c4a076675c5d38f7934e3780dd
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-10-17 14:38:01 +0000

    Add CustomInput Example to the TestApp

---
+++ Tests/TestApp/Client/Features/Color/Pages/CustomInputExamplePage.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApp.Client.Pages
+{
+  using System;
+  using TestApp.Client.Features.Base.Components;
+
+  public partial class CustomInputExamplePage : BaseComponent
+  {
+    protected void HandleValidSubmit() => Console.WriteLine("Valid Submit");
+  }
+}
