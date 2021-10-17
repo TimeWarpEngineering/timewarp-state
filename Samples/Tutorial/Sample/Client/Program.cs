235ca2459ea41e10ac30bad220e5c2b1c65a0021
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-10-17 14:49:56 +0000

    Format

--- Samples/Tutorial/Sample/Client/Program.cs
+++ Samples/Tutorial/Sample/Client/Program.cs
@@ -10,16 +10,16 @@ using Microsoft.Extensions.Logging;
 
 namespace Sample.Client
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
+      await builder.Build().RunAsync();
     }
+  }
 }
