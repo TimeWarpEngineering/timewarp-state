236a08b4c96e41772eb68ff26596143bb672d828
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-03-16 00:55:59 +0000

    Start migration from seperate TypeScript project
    To one and eliminated webpack.  This is adding an initializer model and it succesfully calls the beforestart and started methods but there is an error with ReduxDevTools

---
+++ Source/BlazorState/wwwroot/blazor-state.lib.module.ts
@@ -0,0 +1,7 @@
+﻿export function beforeStart(options, extensions) {
+  console.log("****beforeStart timewarp-state ****");
+}
+
+export function afterStarted(blazor) {
+  console.log("****afterStarted  timewarp-state ****");
+}
