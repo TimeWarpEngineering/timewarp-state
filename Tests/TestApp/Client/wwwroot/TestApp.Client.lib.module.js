a90c9e5256d188eb01169e2636a4f2f80edfc813
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-03-23 08:04:01 +0000

    Don't need initialize() function can do it in beforeStart

--- Tests/TestApp/Client/wwwroot/TestApp.Client.lib.module.js
+++ Tests/TestApp/Client/wwwroot/TestApp.Client.lib.module.js
@@ -3,18 +3,12 @@
 const dispatchIncrementCountAction = () => {
   console.log("%cdispatchIncrementCountAction", "color: green");
   const IncrementCountActionName = "TestApp.Client.Features.Counter.CounterState+IncrementCounterAction, TestApp.Client, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
-  //const blazorState = window["BlazorState"];
   blazorState.DispatchRequest(IncrementCountActionName, { amount: 7 });
 };
 
-function initialize() {
-  console.log("Initialize InteropTest.js");
-  window["InteropTest"] = dispatchIncrementCountAction;
-}
-
 export function beforeStart(options, extensions) {
-  initialize();
   console.log("****beforeStart TestApp.Client ****");
+  window["InteropTest"] = dispatchIncrementCountAction;
 }
 
 export function afterStarted(blazor) {
