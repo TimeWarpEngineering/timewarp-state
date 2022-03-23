f2f37bf6c826a38a9b368a5c5690ec59bc5eb9e8
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-03-23 08:37:10 +0000

    remove the noise from this line wasn't needed just lefto over from refactoring.

--- Source/BlazorState.Js/src/BlazorState.ts
+++ Source/BlazorState.Js/src/BlazorState.ts
@@ -3,12 +3,12 @@
 export class BlazorState {
 
   jsonRequestHandler: any;
-  
+
   async DispatchRequest(requestTypeFullName: string, request: any ) {
     const requestAsJson = JSON.stringify(request);
 
     console.log(`Dispatching request of Type ${requestTypeFullName}: ${requestAsJson}`);
-    await (<any>window[<any>JsonRequestHandlerName]).invokeMethodAsync(JsonRequestHandlerMethodName, requestTypeFullName, requestAsJson);
+    await this.jsonRequestHandler.invokeMethodAsync(JsonRequestHandlerMethodName, requestTypeFullName, requestAsJson);
   }
 }
 
