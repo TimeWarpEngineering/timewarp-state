a1c54150b20d8591994024bc6d75ec0453ce15f0
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-04-02 08:25:17 +0000

    Updates to logging.  To use EventIds for all Log messages.

---
+++ Source/BlazorState/Extensions/MethodInfoExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Reflection;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// Use reflection to invoke an Async method.
+/// </summary>
+/// <seealso href="https://stackoverflow.com/questions/39674988/how-to-call-a-generic-async-method-using-reflection"/>
+public static class MethodInfoExtensions
+{
+  public static async Task<object> InvokeAsync(this MethodInfo aMethodInfo, object aObject, params object[] parameters)
+  {
+    dynamic awaitable = aMethodInfo.Invoke(aObject, parameters);
+    await awaitable;
+    return awaitable.GetAwaiter().GetResult();
+  }
+}
---
+++ Source/BlazorState/Features/JavaScriptInterop/InvalidRequestTypeException.cs
@@ -0,0 +1,37 @@
+﻿#nullable enable
+
+namespace BlazorState.Features.JavaScriptInterop;
+using System;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class InvalidRequestTypeException : Exception, ISerializable
+{
+  public string? RequestedTypeAssemblyQualifiedName { get; set; }
+  public InvalidRequestTypeException() : base() { }
+  public InvalidRequestTypeException(string? message) : base(message) { }
+  public InvalidRequestTypeException(string? message, Exception innerException) : base(message, innerException) { }
+  public InvalidRequestTypeException(string? message, string requestedTypeAssemblyQualifiedName) : base(message)
+  {
+    RequestedTypeAssemblyQualifiedName = requestedTypeAssemblyQualifiedName;
+  }
+
+  public override void GetObjectData(SerializationInfo serializationInfo, StreamingContext streamingContext)
+  {
+    base.GetObjectData(serializationInfo, streamingContext);
+    serializationInfo.AddValue("RequestedTypeAssemblyQualifiedName", RequestedTypeAssemblyQualifiedName);
+  }
+
+  public override string Message =>
+    string.IsNullOrWhiteSpace(RequestedTypeAssemblyQualifiedName) ?
+      $"{base.Message}" :
+      $"{base.Message} Requested Type: {RequestedTypeAssemblyQualifiedName}";
+
+  // A constructor is needed for serialization when an
+  // exception propagates from a remoting server to the client.
+  protected InvalidRequestTypeException
+  (
+    SerializationInfo serializationInfo,
+    StreamingContext streamingContext
+  ) : base(serializationInfo, streamingContext) { }
+}
