a2d15d2b223c6edd1e1a43e94c597c89cd655466
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-10-28 00:45:18 +0000

    add the typescript files back

---
+++ Source/BlazorState/wwwroot/js/Constants.ts
@@ -0,0 +1,8 @@
+﻿export const BlazorStateName: string = "BlazorState";
+export const DevToolsName: string = "devTools"
+export const InitializeJavaScriptInteropName: string = "InitializeJavaScriptInterop";
+export const JsonRequestHandlerMethodName: string = "Handle";
+export const JsonRequestHandlerName: string = "jsonRequestHandler";
+export const ReduxDevToolsFactoryName: string = "ReduxDevToolsFactory";
+export const ReduxDevToolsName: string = "reduxDevTools";
+export const ReduxExtensionName: string = "__REDUX_DEVTOOLS_EXTENSION__";
