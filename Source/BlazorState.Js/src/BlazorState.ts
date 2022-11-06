6208eccf170bf074fd1958cbf9d37e2a879d0e10
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-11-06 13:25:27 +0000

    Add back the esproj given the other just won't work

---
+++ Source/BlazorState.Js/src/BlazorState.ts
@@ -0,0 +1,16 @@
+﻿import {  JsonRequestHandlerMethodName, JsonRequestHandlerName } from './Constants.js';
+
+export class BlazorState {
+
+  jsonRequestHandler: any;
+  reduxDevTools: any;
+
+  async DispatchRequest(requestTypeFullName: string, request: any ) {
+    const requestAsJson = JSON.stringify(request);
+
+    console.log(`Dispatching request of Type ${requestTypeFullName}: ${requestAsJson}`);
+    await this.jsonRequestHandler.invokeMethodAsync(JsonRequestHandlerMethodName, requestTypeFullName, requestAsJson);
+  }
+}
+
+export const blazorState = new BlazorState();
