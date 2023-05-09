de8f5ab8ad08b86e84745e53654cf0bed94e00c5
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-05-09 15:21:36 +0700

    Add a Test Project to test the Analyzer.

---
+++ Tests/BlazorStateAnalyzerTest/SampleState.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorStateAnalyzerTest;
+
+using BlazorState;
+
+public class SampleState : IState
+{
+  public Guid Guid => throw new NotImplementedException();
+
+  public void Initialize() => throw new NotImplementedException();
+
+  public class SampleAction : IAction { }
+}
+
+public class InvalidSampleAction : IAction { }
