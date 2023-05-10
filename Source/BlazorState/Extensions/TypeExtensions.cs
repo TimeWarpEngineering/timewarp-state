663dca331ae77a5ceacc6a3c6e1cb80b7882ac79
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-05-10 21:36:59 +0700

    Add Exception if not nested

--- Source/BlazorState/Extensions/TypeExtensions.cs
+++ Source/BlazorState/Extensions/TypeExtensions.cs
@@ -1,14 +1,23 @@
 ﻿#nullable enable
 namespace BlazorState.Extensions;
 
+using MediatR;
+
 public static class TypeExtensions
 {
   public static Type GetEnclosingStateType(this Type type)
   {
+    var name = type.Name;
     while (type != null && !typeof(IState).IsAssignableFrom(type))
     {
       type = type.DeclaringType!; // Not null because of analyzer
     }
+    if (type == null)
+    {
+      throw new NonNestedClassException
+      ($"{name} must be nested in a class that implements {nameof(IState)}");
+    }
+
 
     return type!; // Not null because of analyzer
   }
