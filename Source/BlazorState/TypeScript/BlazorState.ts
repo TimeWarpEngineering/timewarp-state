7d23ee7c75ae8a410cc32e04db6bc2887c385999
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-12-19 22:00:37 +0700

    Add comments to TypeScript

--- Source/BlazorState/TypeScript/BlazorState.ts
+++ Source/BlazorState/TypeScript/BlazorState.ts
@@ -2,13 +2,24 @@
 
 export class BlazorState {
 
+  // Holds the .NET instance of JsonRequestHandler
   jsonRequestHandler: any;
+  
+  // Holds the Redux DevTools instance
   reduxDevTools: any;
 
+  /**
+   * Dispatches a JSON request to the .NET backend.
+   * @param {string} requestTypeFullName - The full name of the request type.
+   * @param {any} request - The request payload.
+   */
   async DispatchRequest(requestTypeFullName: string, request: any) {
+    // Convert the request payload to a JSON string
     const requestAsJson = JSON.stringify(request);
 
     console.log(`Dispatching request of Type ${requestTypeFullName}: ${requestAsJson}`);
+
+    // Invoke the .NET method to handle the request
     await this.jsonRequestHandler.invokeMethodAsync(JsonRequestHandlerMethodName, requestTypeFullName, requestAsJson);
   }
 }
