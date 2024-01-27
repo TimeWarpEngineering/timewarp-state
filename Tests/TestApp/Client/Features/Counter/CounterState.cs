0e2ac50eeef309d969f13bf5f3aeb8f98c1d0fa9
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-01-27 10:45:02 +0700

    add JsonConstructor to support loading state from json

--- Tests/TestApp/Client/Features/Counter/CounterState.cs
+++ Tests/TestApp/Client/Features/Counter/CounterState.cs
@@ -1,6 +1,7 @@
 namespace TestApp.Client.Features.Counter;
 
 using BlazorState;
+using System.Text.Json.Serialization;
 
 public partial class CounterState : State<CounterState>
 {
@@ -8,6 +9,13 @@ public partial class CounterState : State<CounterState>
   public int Count { get; private set; }
 
   public CounterState() { }
+
+  [JsonConstructor]
+  public CounterState(Guid guid, int count)
+  {
+    Guid = guid;
+    Count = count;
+  }
 
   /// <summary>
   /// Set the Initial State
