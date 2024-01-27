02596be82b3f277470662718e1e0540e4d7a3988
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-01-27 09:13:35 +0700

    Temporary debugging code and TODO for PreRender storage.

--- Source/TimeWarpState.Middleware/PersistentState/PersistentStatePostProcessor.cs
+++ Source/TimeWarpState.Middleware/PersistentState/PersistentStatePostProcessor.cs
@@ -7,54 +7,59 @@ using MediatR.Pipeline;
 using Microsoft.Extensions.Logging;
 using System.Reflection;
 
-internal class PersistentStatePostProcessor<TRequest, TResponse>
+public class PersistentStatePostProcessor<TRequest, TResponse>
 (
   ILogger<PersistentStatePostProcessor<TRequest, TResponse>> logger,
-  IStore store,
-  ISessionStorageService sessionStorageService
+  IStore Store,
+  ISessionStorageService SessionStorageService,
+  ILocalStorageService LocalSessionStorageService
 ) : IRequestPostProcessor<TRequest, TResponse>
   where TRequest : notnull, IAction
 {
   private readonly ILogger Logger = logger;
-  private readonly IStore Store = store;
-  private readonly ISessionStorageService SessionStorageService = sessionStorageService;
+
 
   public async Task Process(TRequest aRequest, TResponse response, CancellationToken cancellationToken)
   {
+    // TODO: remove below or replace with proper Logger.Debug
+    Console.WriteLine($"PersistentStatePostProcessor: {typeof(TRequest).FullName}");
     // We want to persist using the method defined by the PersistentStateAttribute
     // Should we inject a common interface IPersistentState/IPersistState?
     // Then the user would register the IPersistentState with a name and the method.
     // Then we could have a dictionary of IPersistState and call the appropriate one based on PersistentStateMethod?
     // Or we could have the IPersistState.Persist method take the PersistentStateMethod as a parameter?
 
-    Type requestType = typeof(TRequest);
-    Type currentType = requestType;
+    Type currentType = typeof(TRequest);
     while (currentType.DeclaringType != null)
     {
       currentType = currentType.DeclaringType;
     }
     // currentType is now the top-level non-nested type
 
-    // Get the PersistentStateAttribute
     PersistentStateAttribute? persistentStateAttribute =
-      currentType?.GetCustomAttribute<PersistentStateAttribute>();
+      currentType.GetCustomAttribute<PersistentStateAttribute>();
       
     if (persistentStateAttribute is null) return;
 
-    object state = Store.GetStateAsync(currentType);
+    object state = Store.GetState(currentType);
 
-    // switch on the PersistentStateMethod
     switch (persistentStateAttribute.PersistentStateMethod)
     {
       case PersistentStateMethod.Server:
         break;
       case PersistentStateMethod.SessionStorage:
-        Console.WriteLine("Save to Session Storage");
-        await SessionStorageService.SetItemAsync(currentType!.Name, state, cancellationToken);
+        Console.WriteLine($"Save to Session Storage {currentType.Name}");
+        await SessionStorageService.SetItemAsync(currentType.Name, state, cancellationToken);
         break;
       case PersistentStateMethod.LocalStorage:
-        Console.WriteLine("Save to Local Storage");
+        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Save to Local Storage {currentType.Name}");
+        await LocalSessionStorageService.SetItemAsync(currentType.Name, state, cancellationToken);
         break;
+      case PersistentStateMethod.PreRender:
+        // TODO: This needs to be tried and see if improves UX.
+        break;
+      default:
+        throw new ArgumentOutOfRangeException();
     }
   }
 }
