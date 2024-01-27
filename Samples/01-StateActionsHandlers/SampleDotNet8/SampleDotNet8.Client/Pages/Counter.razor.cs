31cd45d51d4c2a0da1847a9e43fc9e708c1a8e89
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-01-27 10:17:40 +0700

    I have moved the PersistenState stuff to the base class so removing here.

--- Samples/01-StateActionsHandlers/SampleDotNet8/SampleDotNet8.Client/Pages/Counter.razor.cs
+++ Samples/01-StateActionsHandlers/SampleDotNet8/SampleDotNet8.Client/Pages/Counter.razor.cs
@@ -1,47 +1,14 @@
 namespace SampleDotNet8.Client.Pages;
 
-using BlazorState;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SampleDotNet8.Client.Features.Counter;
-using System;
 using System.Threading.Tasks;
 
-public partial class Counter : IDisposable
+public partial class Counter
 {
-  [Inject] PersistentComponentState PersistentComponentState { get; set; }
-  [Inject] private IStore Store { get; set; }
+  [Inject] private IStore Store { get; set; } = null!;
 
-  private PersistingComponentStateSubscription PersistingComponentStateSubscription;
   CounterState CounterState => GetState<CounterState>();
 
-  async Task IncrementCount()
-  {
-    await Mediator.Send(new CounterState.IncrementCount.Action { Amount = 5 });
-  }
-
-  protected override async Task OnInitializedAsync()
-  {
-    await base.OnInitializedAsync();
-    PersistingComponentStateSubscription = PersistentComponentState.RegisterOnPersisting(OnPersisting);
-
-    bool foundInState =
-      PersistentComponentState.TryTakeFromJson<CounterState>(nameof(CounterState), out CounterState? counterState);
-
-    if (foundInState)
-    {
-      Store.SetState(counterState);
-    }
-  }
-
-  private Task OnPersisting()
-  {
-    PersistentComponentState.PersistAsJson(instance: CounterState, key: nameof(CounterState));
-    return Task.CompletedTask;
-  }
-
-  void IDisposable.Dispose()
-  {
-    PersistingComponentStateSubscription.Dispose();
-  }
+  async Task IncrementCount() => await Mediator.Send(new CounterState.IncrementCount.Action { Amount = 5 });
 }
