ddf416d292c21c541f79f4880a96b230e432ec19
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-01-28 10:58:29 +0700

    Add IsPreRendering

--- Source/BlazorState/Components/BlazorStateComponent.cs
+++ Source/BlazorState/Components/BlazorStateComponent.cs
@@ -1,62 +1,67 @@
 namespace BlazorState;
 
-using MediatR;
-using Microsoft.AspNetCore.Components;
-using System;
 using System.Collections.Concurrent;
 
 /// <summary>
-/// A non required Base Class that injects Mediator and Store.
-/// And exposes StateHasChanged
+///   A non required Base Class that injects Mediator and Store.
+///   And exposes StateHasChanged
 /// </summary>
 /// <remarks>Implements IBlazorStateComponent by Injecting</remarks>
 public class BlazorStateComponent : ComponentBase, IDisposable, IBlazorStateComponent
 {
-  const string IsPreRenderCompleteKey = "IsPreRenderComplete"; 
-  static readonly ConcurrentDictionary<string, int> s_InstanceCounts = new();
+  private const string IsPreRenderCompleteKey = "IsPreRenderComplete";
+  private static readonly ConcurrentDictionary<string, int> s_InstanceCounts = new();
 
   private PersistingComponentStateSubscription PersistingComponentStateSubscription;
-  
+
   public BlazorStateComponent()
   {
     string name = GetType().Name;
-    int count = s_InstanceCounts.AddOrUpdate(name, 1, (aKey, aValue) => aValue + 1);
+    int count = s_InstanceCounts.AddOrUpdate(name, 1, updateValueFactory: (aKey, aValue) => aValue + 1);
 
     Id = $"{name}-{count}";
   }
 
   /// <summary>
-  /// A generated unique Id based on the Class name and number of times they have been created
+  ///   Indicates if the component is being prerendered.
   /// </summary>
-  public string Id { get; }
-  
-  public bool IsPreRendering { get; private set; } = true;
+  protected bool IsPreRendering { get; private set; } = true;
 
   /// <summary>
-  /// Allows for the Assigning of a value one can use to select an element during automated testing.
+  ///   Allows for the Assigning of a value one can use to select an element during automated testing.
   /// </summary>
   [Parameter] public string TestId { get; set; }
 
-  [Inject] public IMediator Mediator { get; set; } = null!;
   [Inject] private IStore Store { get; set; } = null!;
   [Inject] private RenderPhaseService RenderPhaseService { get; set; } = null!;
+  [Inject] private PersistentComponentState PersistentComponentState { get; set; } = null!;
+  [Inject] protected IMediator Mediator { get; set; } = null!;
   
-  [Inject] PersistentComponentState PersistentComponentState { get; set; } = null!;
-
   /// <summary>
-  /// Maintains all components that subscribe to a State.
-  /// Is updated by using the GetState method
+  ///   Maintains all components that subscribe to a State.
+  ///   Is updated by using the GetState method
   /// </summary>
   [Inject] public Subscriptions Subscriptions { get; set; }
 
   /// <summary>
-  /// Exposes StateHasChanged
+  ///   A generated unique Id based on the Class name and number of times they have been created
   /// </summary>
-  public void ReRender() => base.InvokeAsync(StateHasChanged);
+  public string Id { get; }
 
   /// <summary>
-  /// Place a Subscription for the calling component
-  /// And returns the requested state
+  ///   Exposes StateHasChanged
+  /// </summary>
+  public void ReRender() => InvokeAsync(StateHasChanged);
+  public virtual void Dispose()
+  {
+    PersistingComponentStateSubscription.Dispose();
+    Subscriptions.Remove(this);
+    GC.SuppressFinalize(this);
+  }
+
+  /// <summary>
+  ///   Place a Subscription for the calling component
+  ///   And returns the requested state
   /// </summary>
   /// <typeparam name="T"></typeparam>
   /// <returns></returns>
@@ -70,23 +75,28 @@ public class BlazorStateComponent : ComponentBase, IDisposable, IBlazorStateComp
   protected override async Task OnInitializedAsync()
   {
     await base.OnInitializedAsync();
-    
-    PersistingComponentStateSubscription = PersistentComponentState.RegisterOnPersisting(Persist);
-    
-    bool foundInState =
-      PersistentComponentState.TryTakeFromJson<bool>(IsPreRenderCompleteKey, out bool _);
-    
-    if (foundInState) { IsPreRendering = false; }
-    
+    if (OperatingSystem.IsBrowser())
+    {
+      IsPreRendering = false;
+    }
+    else
+    {
+      PersistingComponentStateSubscription = PersistentComponentState.RegisterOnPersisting(Persist);
+
+      bool foundInState =
+        PersistentComponentState.TryTakeFromJson(IsPreRenderCompleteKey, out bool _);
+
+      if (foundInState) { IsPreRendering = false; }
+    }
   }
 
   private Task Persist()
   {
     // This will fire only once just prior to transitioning to wasm. 
     PersistentComponentState.PersistAsJson("IsPreRenderComplete", true);
-    return Task.CompletedTask;    
+    return Task.CompletedTask;
   }
-  
+
   protected override void OnAfterRender(bool firstRender)
   {
     base.OnAfterRender(firstRender);
@@ -94,11 +104,5 @@ public class BlazorStateComponent : ComponentBase, IDisposable, IBlazorStateComp
     {
       RenderPhaseService.SetInteractive();
     }
-  }
-  public virtual void Dispose()
-  {
-    PersistingComponentStateSubscription.Dispose();
-    Subscriptions.Remove(this);
-    GC.SuppressFinalize(this);
   }
 }
