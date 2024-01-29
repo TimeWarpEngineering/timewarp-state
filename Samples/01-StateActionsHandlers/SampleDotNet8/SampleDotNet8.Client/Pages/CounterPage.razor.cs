7a1dc65dfe5f53cd58edc92989afee00416f5b3f
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-01-29 08:37:01 +0700

    Add reference CounterState2 to the Counterpage

--- Samples/01-StateActionsHandlers/SampleDotNet8/SampleDotNet8.Client/Pages/CounterPage.razor.cs
+++ Samples/01-StateActionsHandlers/SampleDotNet8/SampleDotNet8.Client/Pages/CounterPage.razor.cs
@@ -2,13 +2,16 @@ namespace SampleDotNet8.Client.Pages;
 
 using Microsoft.AspNetCore.Components;
 using SampleDotNet8.Client.Features.Counter;
+using SampleDotNet8.Client.Features.Counter2;
 using System.Threading.Tasks;
 
-public partial class Counter
+public partial class CounterPage
 {
   [Inject] private IStore Store { get; set; } = null!;
 
   CounterState CounterState => GetState<CounterState>();
+  Counter2State Counter2State => GetState<Counter2State>();
 
   async Task IncrementCount() => await Mediator.Send(new CounterState.IncrementCount.Action { Amount = 5 });
+  async Task IncrementCount2() => await Mediator.Send(new Counter2State.IncrementCount.Action { Amount = 3 });
 }
