aa331a03f573d1c6273467e0273ff971a5411429
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-02 22:21:57 +0700

    Update sln

--- Tests/Client.Integration.Tests/GlobalSuppressions.cs
+++ Tests/Client.Integration.Tests/GlobalSuppressions.cs
@@ -3,4 +3,4 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~F:TestApp.Client.Integration.Tests.StoreTests.ReduxDevToolsStore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~F:TestApp.Test.App.Client.Integration.Tests.StoreTests.ReduxDevToolsStore")]
