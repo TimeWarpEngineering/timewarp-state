6d1f6d25dec49ec2cf82eac7eaf95aab32551658
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-20 13:02:39 +0700

    cleanup and Global Usings

--- Tests/Test.App/Test.App.Client/Features/WeatherForecast/Actions/Fetch/FetchWeatherForecastsHandler.cs
+++ Tests/Test.App/Test.App.Client/Features/WeatherForecast/Actions/Fetch/FetchWeatherForecastsHandler.cs
@@ -1,9 +1,5 @@
 namespace Test.App.Client.Features.WeatherForecast;
 
-using System.Net.Http;
-using System.Net.Http.Json;
-using Test.App.Contracts.Features.WeatherForecast;
-
 internal partial class WeatherForecastsState
 {
   public class FetchWeatherForecastsHandler
@@ -28,11 +24,9 @@ internal partial class WeatherForecastsState
           cancellationToken: aCancellationToken
         );
       
-      if (getWeatherForecastsResponse is null)
-      {
-        throw new System.ArgumentNullException(null, nameof(getWeatherForecastsResponse));
-      }
-      WeatherForecastsState._WeatherForecasts = getWeatherForecastsResponse.WeatherForecasts;
+      ArgumentNullException.ThrowIfNull(getWeatherForecastsResponse);
+      
+      WeatherForecastsState.WeatherForecastList = getWeatherForecastsResponse.WeatherForecasts;
     }
   }
 }
