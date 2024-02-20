4f742914ca28f8b85e36e16c40e90537d2973bc2
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-20 12:24:55 +0700

    move to global usings

--- Tests/Test.App/Test.App.Client/Features/EventStream/Components/EventStream.razor.cs
+++ Tests/Test.App/Test.App.Client/Features/EventStream/Components/EventStream.razor.cs
@@ -1,7 +1,5 @@
 namespace Test.App.Client.Features.EventStream.Components;
 
-using System.Collections.Generic;
-
 public partial class EventStream : BaseComponent
 {
   private IReadOnlyList<string> Events => EventStreamState.Events;
