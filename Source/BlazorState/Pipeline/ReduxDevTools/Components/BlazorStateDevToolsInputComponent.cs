e065dc2f6645c1c92d1d66708de1f1934d1f0030
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-20 20:47:52 +0700

    This also should be abstract class

--- Source/BlazorState/Pipeline/ReduxDevTools/Components/BlazorStateDevToolsInputComponent.cs
+++ Source/BlazorState/Pipeline/ReduxDevTools/Components/BlazorStateDevToolsInputComponent.cs
@@ -9,7 +9,7 @@ namespace BlazorState.Pipeline.ReduxDevTools;
 /// They won't be forced to use this one.
 /// C# 8 with default implementations of interfaces will be quite tempting to solve this.
 /// </remarks>
-public class BlazorStateDevToolsInputComponent<T> : BlazorStateInputComponent<T>,
+public abstract class BlazorStateDevToolsInputComponent<T> : BlazorStateInputComponent<T>,
   IDevToolsComponent
 {
   protected override void OnInitialized() => Subscriptions.Add<IDevToolsComponent>(this);
