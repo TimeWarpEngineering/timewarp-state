9904a1b554a5cb7e3063799271f914603f63a04b
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-20 11:25:09 +0700

    display response type in example.

--- Tests/Test.App/Test.App.Client/Features/Counter/Notification/IncrementCountNotificationHandler.cs
+++ Tests/Test.App/Test.App.Client/Features/Counter/Notification/IncrementCountNotificationHandler.cs
@@ -1,14 +1,11 @@
 namespace Test.App.Client.Features.Counter;
 
 internal class IncrementCountNotificationHandler
-  : INotificationHandler<PostPipelineNotification<CounterState.IncrementCounterAction, Unit>>
+(
+  ILogger<IncrementCountNotificationHandler> logger
+) : INotificationHandler<PostPipelineNotification<CounterState.IncrementCounterAction, Unit>>
 {
-  private readonly ILogger Logger;
-
-  public IncrementCountNotificationHandler(ILogger<IncrementCountNotificationHandler> aLogger)
-  {
-    Logger = aLogger;
-  }
+  private readonly ILogger Logger = logger;
 
   public Task Handle
   (
@@ -17,6 +14,7 @@ internal class IncrementCountNotificationHandler
   )
   {
     Logger.LogDebug("{aPostPipelineNotification_Request_Type_Name}", aPostPipelineNotification.Request.GetType().Name);
+    Logger.LogDebug("{aPostPipelineNotification_Response_Type_Name}", aPostPipelineNotification.Response.GetType().Name);
     Logger.LogDebug("{methodName} handled", nameof(IncrementCountNotificationHandler));
     return Task.CompletedTask;
   }
