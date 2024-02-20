74328d94eb2f17bad555a8f82a4261dfb313492d
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-20 12:19:35 +0700

    naming convention

--- Source/BlazorState/State/State.cs
+++ Source/BlazorState/State/State.cs
@@ -2,19 +2,18 @@ namespace BlazorState;
 
 public abstract class State<TState> : IState<TState>
 {
-
   [IgnoreDataMember]
-  public Guid Guid { get; protected set; } = Guid.NewGuid();
+  public Guid Guid { get; protected init; } = Guid.NewGuid();
 
   TState IState<TState>.State { get; }
 
   /// <summary>
   /// returns a new instance of type TState
   /// </summary>
-  /// <param name="aKeyValuePairs">Initialize the TState instance with these values</param>
+  /// <param name="keyValuePairs">Initialize the TState instance with these values</param>
   /// <returns>The particular State of type TState</returns>
   /// <remarks>Implement this if you want to use ReduxDevTools Time Travel</remarks>
-  public virtual TState Hydrate(IDictionary<string, object> aKeyValuePairs) => throw new NotImplementedException();
+  public virtual TState Hydrate(IDictionary<string, object> keyValuePairs) => throw new NotImplementedException();
 
   /// <summary>
   /// Use this method to prevent running methods from source other than Tests
