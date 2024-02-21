a8994c89c948ca14317ecdec6cb97496f32e127a
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-21 11:50:03 +0700

    Add Generated Files to the repo.  This helps get insights into what the SourceGenerators do.

---
+++ Tests/Test.App/Test.App.Client/Generated/TimeWarp.State.SourceGenerator/TimeWarp.State.SourceGenerator.PersistenceStateSourceGenerator/Test.App.Client.Features.Blue.BlueState_Persistence.g.cs
@@ -0,0 +1,36 @@
+﻿#nullable enable
+
+#pragma warning disable CS1591
+namespace Test.App.Client.Features.Blue;
+
+public partial class BlueState
+{
+    // ReSharper disable once UnusedType.Global
+    public static class Load
+    {
+        // ReSharper disable once UnusedType.Global
+        public class Action : IAction { }
+
+        // ReSharper disable once UnusedType.Global
+        public class Handler
+        (
+          IStore store,
+          IPersistenceService PersistenceService
+        ): ActionHandler<Action>(store)
+        {
+            public override async System.Threading.Tasks.Task Handle(Action aAction, System.Threading.CancellationToken aCancellationToken)
+            {
+                try
+                {
+                    object? state = await PersistenceService.LoadState(typeof(BlueState), PersistentStateMethod.SessionStorage);
+                    if (state is BlueState blueState) Store.SetState(blueState);
+                }
+                catch (Exception)
+                {
+                    // if this is a JavaScript not available exception then we are prerendering and just swallow it
+                }
+            }
+        }
+    }
+}
+#pragma warning restore CS1591
---
+++ Tests/Test.App/Test.App.Client/Generated/TimeWarp.State.SourceGenerator/TimeWarp.State.SourceGenerator.PersistenceStateSourceGenerator/Test.App.Client.Features.Purple.PurpleState_Persistence.g.cs
@@ -0,0 +1,36 @@
+﻿#nullable enable
+
+#pragma warning disable CS1591
+namespace Test.App.Client.Features.Purple;
+
+public partial class PurpleState
+{
+    // ReSharper disable once UnusedType.Global
+    public static class Load
+    {
+        // ReSharper disable once UnusedType.Global
+        public class Action : IAction { }
+
+        // ReSharper disable once UnusedType.Global
+        public class Handler
+        (
+          IStore store,
+          IPersistenceService PersistenceService
+        ): ActionHandler<Action>(store)
+        {
+            public override async System.Threading.Tasks.Task Handle(Action aAction, System.Threading.CancellationToken aCancellationToken)
+            {
+                try
+                {
+                    object? state = await PersistenceService.LoadState(typeof(PurpleState), PersistentStateMethod.LocalStorage);
+                    if (state is PurpleState purpleState) Store.SetState(purpleState);
+                }
+                catch (Exception)
+                {
+                    // if this is a JavaScript not available exception then we are prerendering and just swallow it
+                }
+            }
+        }
+    }
+}
+#pragma warning restore CS1591
