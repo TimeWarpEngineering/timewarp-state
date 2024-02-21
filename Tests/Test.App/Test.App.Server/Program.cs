eb3fd192865856cd67764061cefacc5da2c7c7a8
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-21 11:48:39 +0700

    replace var

--- Tests/Test.App/Test.App.Server/Program.cs
+++ Tests/Test.App/Test.App.Server/Program.cs
@@ -5,14 +5,14 @@ internal class Program
 {
   private static void Main(string[] args)
   {
-    var builder = WebApplication.CreateBuilder(args);
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
     // Add services to the container.
     builder.Services.AddRazorComponents()
         .AddInteractiveServerComponents()
         .AddInteractiveWebAssemblyComponents();
     Test.App.Client.Program.ConfigureServices(builder.Services);
-    var app = builder.Build();
+    WebApplication app = builder.Build();
 
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
@@ -37,5 +37,6 @@ internal class Program
         .AddAdditionalAssemblies(typeof(Test.App.Client._Imports).Assembly);
 
     app.Run();
+    
   }
 }
