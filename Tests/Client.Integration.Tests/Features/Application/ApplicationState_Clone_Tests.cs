3ab1a9e9774dd3c1c907c20d4565b9b7984c86e4
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-22 19:48:07 +0700

    Add Clone tests

---
+++ Tests/Client.Integration.Tests/Features/Application/ApplicationState_Clone_Tests.cs
@@ -0,0 +1,27 @@
+// ReSharper disable UnusedType.Global
+namespace ApplicationState_;
+
+public class Clone_Should : BaseTest
+{
+  public Clone_Should(ClientHost aWebAssemblyHost) : base(aWebAssemblyHost)
+  {
+    ApplicationState = Store.GetState<ApplicationState>();
+  }
+
+  private ApplicationState ApplicationState { get; set; }
+  
+  public void Clone()
+  {
+    //Arrange
+    ApplicationState.Initialize(name: "TestName", exceptionMessage: "Some ExceptionMessage");
+
+    //Act
+    ApplicationState clone = ApplicationState.Clone();
+
+    //Assert
+    ApplicationState.Should().NotBeSameAs(clone);
+    ApplicationState.Name.Should().Be(clone.Name);
+    ApplicationState.ExceptionMessage.Should().Be(clone.ExceptionMessage);
+    ApplicationState.Guid.Should().NotBe(clone.Guid);
+  }
+}
