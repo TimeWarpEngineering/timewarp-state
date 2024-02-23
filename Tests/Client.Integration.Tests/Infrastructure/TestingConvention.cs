d99572591a8fb011a40734e4389ca1d79e344117
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-23 19:07:57 +0700

    Bring in TestingConvention.cs from TestApp.
    This could be a bit better if we used TestApplications like in TimeWarp Architecture.  Revisit later

--- Tests/Client.Integration.Tests/Infrastructure/TestingConvention.cs
+++ Tests/Client.Integration.Tests/Infrastructure/TestingConvention.cs
@@ -1,3 +1,43 @@
 namespace Client.Integration.Tests.Infrastructure;
 
-public class TestingConvention: TimeWarp.Fixie.TestingConvention { }
+[UsedImplicitly]
+public class TestingConvention() : TimeWarp.Fixie.TestingConvention(ConfigureAdditionalServicesCallback)
+{
+  private static void ConfigureAdditionalServicesCallback(ServiceCollection serviceCollection)
+  {
+    var serverWebApplicationFactory = new WebApplicationFactory<Test.App.Server.Program>();
+    HttpClient serverHttpClient = serverWebApplicationFactory.CreateClient();
+  
+    ConfigureWebAssemblyHost(serviceCollection, serverHttpClient);
+  
+    serviceCollection.AddSingleton(new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+  }
+  
+  private static void ConfigureWebAssemblyHost(IServiceCollection serviceCollection, HttpClient serverHttpClient)
+  {
+    var clientHostBuilder = ClientHostBuilder.CreateDefault();
+    ConfigureServices(clientHostBuilder.Services, serverHttpClient);
+  
+    ClientHost clientHost = clientHostBuilder.Build();
+    serviceCollection.AddSingleton(clientHost);
+  }
+  
+  private static void ConfigureServices(IServiceCollection serviceCollection, HttpClient serverHttpClient)
+  {
+    // Need an HttpClient to talk to the Server side configured before calling AddBlazorState.
+    serviceCollection.AddSingleton(serverHttpClient);
+    serviceCollection.AddBlazorState
+    (
+      aOptions => aOptions.Assemblies =
+        new[] { typeof(Test.App.Client.Program).GetTypeInfo().Assembly }
+    );
+  
+    serviceCollection.AddSingleton
+    (
+      new JsonSerializerOptions
+      {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+      }
+    );
+  }
+}
