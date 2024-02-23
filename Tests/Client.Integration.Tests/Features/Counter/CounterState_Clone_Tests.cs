4fdbb94183b4b67f26608fec90bc1a7e0fa26b25
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-23 19:35:52 +0700

    Add in CounterState Clone tests

---
+++ Tests/Client.Integration.Tests/Features/Counter/CounterState_Clone_Tests.cs
@@ -0,0 +1,28 @@
+namespace CounterState;
+
+using Test.App.Client.Features.Counter;
+
+[UsedImplicitly]
+public class Clone_Should : BaseTest
+{
+  public Clone_Should(ClientHost aWebAssemblyHost) : base(aWebAssemblyHost)
+  {
+    CounterState = Store.GetState<CounterState>();
+  }
+
+  private CounterState CounterState { get; set; }
+
+  public void Clone()
+  {
+    //Arrange
+    CounterState.Initialize(count: 15);
+
+    //Act
+    CounterState? clone = CounterState.Clone();
+
+    //Assert
+    CounterState.Should().NotBeSameAs(clone);
+    CounterState.Count.Should().Be(clone.Count);
+    CounterState.Guid.Should().NotBe(clone.Guid);
+  }
+}
