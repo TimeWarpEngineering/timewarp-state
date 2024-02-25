5c6a57a146c68c7f8471af4b5de005a70d7f98b0
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-25 22:45:17 +0700

    Cleaning up Test Pages using Arrange Act Assert pattern

--- Tests/Test.App/Test.App.Client/Features/EventStream/Components/EventStream.razor.cs
+++
@@ -1,6 +0,0 @@
-namespace Test.App.Client.Features.EventStream.Components;
-
-public partial class EventStream : BaseComponent
-{
-  private IReadOnlyList<string> Events => EventStreamState.Events;
-}
--- Tests/Test.App/Test.App.Client/Features/Purple/Components/PurpleCounter.razor.cs
+++ Tests/Test.App/Test.App.Client/Features/Purple/Components/PurpleCounter.razor.cs
@@ -3,13 +3,5 @@ namespace Test.App.Client.Features.Purple.Components;
 [UsedImplicitly]
 public partial class PurpleCounter
 {
-  [Inject] private ILogger<PurpleCounter> Logger { get; set; } = null!;
-  private PurpleState PurpleState => GetState<PurpleState>();
-  private async Task IncrementCount() => await Mediator.Send(new PurpleState.IncrementCount.Action { Amount = 5 });
 
-  protected override void OnAfterRender(bool firstRender)
-  {
-    Logger.LogInformation("{TypeName}:OnAfterRender", GetType().Name);
-    base.OnAfterRender(firstRender);
-  }
 }
