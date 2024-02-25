817ca9ea344507f86436686839d94ef18d7afd40
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-25 23:59:15 +0700

    Add Color and Theme Features

---
+++ Tests/Test.App/Test.App.Client/Features/Color/ColorState.Debug.cs
@@ -0,0 +1,31 @@
+namespace Test.App.Client.Features.Counter;
+
+using System.Drawing;
+
+internal partial class ColorState
+{
+  public override ColorState Hydrate(IDictionary<string, object> keyValuePairs)
+  {
+    var colorState = new ColorState
+    {
+      FavoriteColor = (Color)keyValuePairs[CamelCase.MemberNameToCamelCase(nameof(FavoriteColor))],
+      MyColorName = (string)keyValuePairs[CamelCase.MemberNameToCamelCase(nameof(FavoriteColor))],
+      Guid = 
+        new Guid
+        (
+          keyValuePairs[CamelCase.MemberNameToCamelCase(nameof(Guid))].ToString() ?? 
+            throw new InvalidOperationException()
+        )
+    };
+    
+    return colorState;
+  }
+
+  [UsedImplicitly]
+  public void Initialize(Color color, string myColorName)
+  {
+    ThrowIfNotTestAssembly(Assembly.GetCallingAssembly());
+    FavoriteColor = color;
+    MyColorName = myColorName;
+  }
+}
