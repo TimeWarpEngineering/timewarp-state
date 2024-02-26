674a3c109b33bc98a6f0df0d30849d65ba22b85f
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-02-26 14:07:01 +0700

    Update BaseComponent.cs and clean up

--- Tests/Test.App/Test.App.Client/Features/Base/Components/BaseComponent.cs
+++ Tests/Test.App/Test.App.Client/Features/Base/Components/BaseComponent.cs
@@ -1,5 +1,7 @@
 namespace Test.App.Client.Features.Base.Components;
 
+using CloneTest;
+
 /// <summary>
 /// Makes access to the State a little easier and by inheriting from
 /// BlazorStateDevToolsComponent it allows for ReduxDevTools operation.
@@ -11,12 +13,15 @@ namespace Test.App.Client.Features.Base.Components;
 /// </remarks>
 public abstract class BaseComponent : BlazorStateDevToolsComponent
 {
-  internal RouteState RouteState => GetState<RouteState>();
   protected ApplicationState ApplicationState => GetState<ApplicationState>();
+  internal BlueState BlueState => GetState<BlueState>();
   internal CounterState CounterState => GetState<CounterState>();
+  internal CloneableState CloneableState => GetState<CloneableState>();
   internal EventStreamState EventStreamState => GetState<EventStreamState>();
-  internal WeatherForecastsState WeatherForecastsState => GetState<WeatherForecastsState>();
+  internal PurpleState PurpleState => GetState<PurpleState>();
+  internal RouteState RouteState => GetState<RouteState>();
   internal ThemeState ThemeState => GetState<ThemeState>();
+  internal WeatherForecastsState WeatherForecastsState => GetState<WeatherForecastsState>();
   
   // ReSharper disable once UnusedMember.Global - Can be used by inheriting classes not in this library.
   protected Task Send(IRequest aRequest) => Mediator.Send(aRequest);
