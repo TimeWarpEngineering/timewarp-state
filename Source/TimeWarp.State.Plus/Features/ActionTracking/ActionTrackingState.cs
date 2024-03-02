4417b4a9aa942f5b2f7f3ecfdc96ddfa67caa2e2
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-03 01:04:48 +0700

    IsActive and IsAnyActive now are the methods... I think this all reads much better.

--- Source/TimeWarp.State.Plus/Features/ActionTracking/ActionTrackingState.cs
+++ Source/TimeWarp.State.Plus/Features/ActionTracking/ActionTrackingState.cs
@@ -3,8 +3,8 @@ namespace TimeWarp.Features.ActionTracking;
 public partial class ActionTrackingState : State<ActionTrackingState>
 {
   private List<IAction> ActiveActionsList = [];
-  public bool IsProcessing => ActiveActionsList.Count > 0;
-  public bool IsProcessingAny(params Type[] actionTypes) => 
+  public bool IsActive => ActiveActionsList.Count > 0;
+  public bool IsAnyActive(params Type[] actionTypes) => 
     ActiveActionsList.Any(action => actionTypes.Any(type => type.IsInstanceOfType(action)));
 
   public IReadOnlyList<IAction> ActiveActions => ActiveActionsList.AsReadOnly();
