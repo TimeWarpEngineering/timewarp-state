6ad9bb91427d1c4f147428b58e4b7e14a4c28c03
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-05 08:38:44 +0700

    Update AssemlyMarkers and set strategy.

---
+++ Tests/Test.App/Test.App.Client/AssemblyMarker.cs
@@ -0,0 +1,12 @@
+namespace Test.App.Client;
+
+/// <summary>
+/// Serves as a marker for the assembly, facilitating easy identification and reflection-based operations.
+/// </summary>
+/// <remarks>
+/// This class is intended to be used as a reference point within the assembly for scenarios such as assembly scanning,
+/// where a stable, known type is required to locate the assembly at runtime. The class is sealed to indicate it is not
+/// designed for inheritance or extension, reinforcing its role as a simple marker.
+/// </remarks>
+[UsedImplicitly]
+public sealed class AssemblyMarker { }
