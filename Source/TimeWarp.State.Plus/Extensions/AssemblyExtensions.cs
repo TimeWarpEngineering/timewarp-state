2625865658a90237f76dcc0e9d4e83511602e523
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-06 17:52:00 +0700

    Add AssemblyInfo class and extension method to get Commit Hash and Date

--- Source/TimeWarp.State.Plus/Extensions/AssemblyExtensions.cs
+++ Source/TimeWarp.State.Plus/Extensions/AssemblyExtensions.cs
@@ -1,7 +1,29 @@
 namespace TimeWarp.State.Plus.Extensions;
 
+[UsedImplicitly]
 public static class AssemblyExtensions
 {
-  public static string? GetInformationalVersion(this Assembly assembly) => 
-    assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+  [UsedImplicitly]
+  public static AssemblyInfo GetAssemblyInfo(this Assembly assembly) => new(assembly);
+}
+
+/// <summary>
+/// Represents information extracted from an assembly's metadata attributes.
+/// </summary>
+public class AssemblyInfo
+{
+  private readonly Dictionary<string, string?> Metadata;
+
+  public AssemblyInfo(Assembly assembly)
+  {
+    Metadata = assembly
+      .GetCustomAttributes<AssemblyMetadataAttribute>()
+      .ToDictionary(attr => attr.Key, attr => attr.Value);
+  }
+
+  private string? GetMetadata(string key) => Metadata.GetValueOrDefault(key);
+
+  // Convenience properties
+  [UsedImplicitly] public string? CommitHash => GetMetadata("CommitHash");
+  [UsedImplicitly] public string? CommitDate => GetMetadata("CommitDate");
 }
