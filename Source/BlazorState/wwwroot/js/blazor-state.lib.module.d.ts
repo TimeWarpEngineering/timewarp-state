f756b028744a4219e7eb953efe28f7eace28fdde
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-12 12:59:55 +0700

    delete the generated files an put it back in .gitignore.

--- Source/BlazorState/wwwroot/js/blazor-state.lib.module.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function beforeWebStart(_options: any, _extensions: any): void;
-export declare function afterWebStarted(_blazor: any): void;
-export declare function beforeWebAssemblyStart(_options: any, _extensions: any): void;
-export declare function afterWebAssemblyStarted(_blazor: any): void;
-export declare function beforeServerStart(_options: any, _extensions: any): void;
-export declare function afterServerStarted(_blazor: any): void;
