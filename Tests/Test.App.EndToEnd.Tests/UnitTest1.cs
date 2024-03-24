6153d468ef87fb72187eef4df04b1b9058217ba2
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-24 11:25:24 +0700

    Use the Getting Started test

--- Tests/Test.App.EndToEnd.Tests/UnitTest1.cs
+++ Tests/Test.App.EndToEnd.Tests/UnitTest1.cs
@@ -1,15 +1,23 @@
 namespace Test.App.EndToEnd.Tests;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
+using Microsoft.Playwright.NUnit;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class MyTest : PageTest
+{
+  [Test]
+  public async Task ShouldHaveTheCorrectSlogan()
+  {
+    await Page.GotoAsync("https://playwright.dev");
+    await Expect(Page.Locator("text=enables reliable end-to-end testing for modern web apps")).ToBeVisibleAsync();
+  }
+
+  [Test]
+  public async Task ShouldHaveTheCorrectTitle()
+  {
+    await Page.GotoAsync("https://playwright.dev");
+    var title = Page.Locator(".navbar__inner .navbar__title");
+    await Expect(title).ToHaveTextAsync("Playwright");
+  }
 }
