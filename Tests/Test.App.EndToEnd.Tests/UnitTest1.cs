5e6203d225b5bbadb477c750e0e649975c431ade
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-24 11:06:03 +0700

    dotnet new nunit -n Test.App.EndToEnd.Tests

---
+++ Tests/Test.App.EndToEnd.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Test.App.EndToEnd.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
