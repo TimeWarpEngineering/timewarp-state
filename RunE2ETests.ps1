401a6e2df76436bb97c70acc23a19a8fdf9f2ac5
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-25 16:57:58 +0700

    Trying to get dotcover to do it all.

--- RunE2ETests.ps1
+++ RunE2ETests.ps1
@@ -1,20 +1,29 @@
 Push-Location $PSScriptRoot
-try
-{
+try {
   Push-Location .\Tests\Test.App.EndToEnd.Tests
-  $settings = "chrome.runsettings", "edge.runsettings", "firefox.runsettings", "webkit.runsettings"
-  foreach ($setting in $settings)
-  {
-    dotnet test --settings:$setting --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format="cobertura"
+  $settings = @("chrome.runsettings", "edge.runsettings", "webkit.runsettings")
+  $snapshots = @()
+
+  foreach ($setting in $settings) {
+    $snapshot = "coverage{0}.snapshot" -f $setting.Replace(".runsettings", "")
+    # dotnet dotcover cover --output=$snapshot --DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format="cobertura" --settings=$setting --targetExecutable="dotnet" --targetArguments="test --no-build --settings:$setting"
+    dotnet dotCover cover .\dotcover.config.xml --output=Output\$snapshot --targetArguments="test --no-build --settings:PlaywrightSettings\$setting"
+    $snapshots += ".\Output\$snapshot"
   }
 
-  $coverageFiles = Get-ChildItem -Recurse -Filter "coverage.cobertura.xml" | ForEach-Object { $_.FullName }
-  $coverageFilesArgs = $coverageFiles -join ";"
-  & reportGenerator "-reports:$coverageFilesArgs" "-targetdir:coveragereport" "-reportTypes:Html;MarkdownSummaryGithub"
+  # Create a new variable for the Source
+  $sourceParameter = $snapshots -join ';'
 
-  .\coveragereport\index.html
+  # Write out the Source parameter to console
+  Write-Host "Source parameter for dotCover merge: $sourceParameter"
+  
+  $mergedSnapshot = "mergedCoverage.snapshot"
+  dotnet dotcover merge --output="Output\$mergedSnapshot" --Source=$sourceParameter
+
+  dotnet dotcover report --source=$mergedSnapshot --output="Output\coverageReport.html" --reportType="HTML"
+
+  .\Output\coverageReport.html
 }
-finally
-{
+finally {
   Pop-Location
 }
