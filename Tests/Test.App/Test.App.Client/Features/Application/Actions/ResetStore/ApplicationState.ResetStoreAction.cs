aff6e5f9f12f91230ddafa77666ff3ff1c848419
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-16 01:29:30 +0700

    Use explicit constructor

--- Tests/Test.App/Test.App.Client/Features/Application/Actions/ResetStore/ApplicationState.ResetStoreAction.cs
+++ Tests/Test.App/Test.App.Client/Features/Application/Actions/ResetStore/ApplicationState.ResetStoreAction.cs
@@ -7,16 +7,19 @@ public partial class ApplicationState
   public class ResetStoreAction : IAction;
   
   [UsedImplicitly]
-  internal class ResetStoreHandler
-  (
-    IStore Store,
-    ISender Sender
-  ) : IRequestHandler<ResetStoreAction>
+  internal class ResetStoreHandler : IRequestHandler<ResetStoreAction>
   {
+    private readonly IStore Store;
+    private readonly ISender Sender;
+    public ResetStoreHandler(IStore store, ISender sender)
+    {
+      Store = store;
+      Sender = sender;
+    }
     public async Task Handle(ResetStoreAction resetStoreAction, CancellationToken cancellationToken)
     {
       Store.Reset();
-      await Sender.Send(new ChangeRoute.Action { NewRoute = "/" }, cancellationToken);
+      await Sender.Send(new ChangeRoute.Action(newRoute:"/"), cancellationToken);
     }
   }
 }
