7497498d03bd32f3ba445eb7170859b99305c7e4
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-16 01:08:36 +0700

    Add AddTimeWarpStateRouting Extension to register the services needed for Routing

---
+++ Source/TimeWarp.State.Plus/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+namespace TimeWarp.State.Plus.Extensions;
+
+using Microsoft.Extensions.DependencyInjection;
+using TimeWarp.Features.Routing;
+using static TimeWarp.Features.Routing.RouteState;
+
+public static class ServiceCollectionExtensions
+{
+  /// <summary>
+  /// Register TimeWarp.State.Plus Routing 
+  /// </summary>
+  /// <param name="serviceCollection"></param>
+  public static IServiceCollection AddTimeWarpStateRouting(this IServiceCollection serviceCollection)
+  {
+    // To avoid duplicate registrations we look to see if one has already been registered.
+    if (serviceCollection.HasRegistrationFor(typeof(RouteState))) return serviceCollection;
+
+    serviceCollection.AddScoped<RouteState>();
+    serviceCollection.AddTransient<IRequestHandler<ChangeRoute.Action>, ChangeRoute.Handler>();
+    serviceCollection.AddTransient<IRequestHandler<GoBack.Action>, GoBack.Handler>();
+    serviceCollection.AddTransient<IRequestHandler<PushRouteInfo.Action>, PushRouteInfo.Handler>();
+
+    return serviceCollection;
+  }
+  
+  private static bool HasRegistrationFor(this IServiceCollection serviceCollection, Type type) =>
+    serviceCollection.Any(serviceDescriptor => serviceDescriptor.ServiceType == type);
+  
+}
