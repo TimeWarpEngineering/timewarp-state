aedbaa2c98645652cfae9a912da0e09bf170aa1b
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-16 21:45:39 +0700

    Make Sample compile although now I think it needs redone wih updated tutorial.

--- Samples/01-StateActionsHandlers/Sample/Client/App.razor.cs
+++ Samples/01-StateActionsHandlers/Sample/Client/App.razor.cs
@@ -1,31 +1,11 @@
 ﻿namespace Sample.Client;
 
-using System.Threading.Tasks;
 using TimeWarp.Features.ReduxDevTools;
 using TimeWarp.Features.JavaScriptInterop;
-using TimeWarp.Features.Routing;
 using Microsoft.AspNetCore.Components;
 
 public partial class App : ComponentBase
 {
   [Inject] private JsonRequestHandler JsonRequestHandler { get; set; } = null!;
   [Inject] private ReduxDevToolsInterop ReduxDevToolsInterop { get; set; } = null!;
-
-  [Inject]
-  [System.Diagnostics.CodeAnalysis.SuppressMessage
-    (
-      "CodeQuality", 
-      "IDE0051:Remove unused private members", 
-      Justification = "It is used, the constructor has side effects "
-    )
-  ]
-  private TimeWarpNavigationManager TimeWarpNavigationManager { get; set; } = null!;
-
-  protected override async Task OnAfterRenderAsync(bool firstRender)
-  {
-    // TODO:
-    throw new Exception("do I hit?"); 
-    await ReduxDevToolsInterop.InitAsync();
-    await JsonRequestHandler.InitAsync();
-  }
 }
