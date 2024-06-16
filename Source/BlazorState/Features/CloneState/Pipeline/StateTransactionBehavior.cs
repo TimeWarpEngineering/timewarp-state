9a3ac08fb46fcc96bd474f58155824dea59c5b5e
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-16 11:00:56 +0700

    Add Required values in constructor

--- Source/BlazorState/Features/CloneState/Pipeline/StateTransactionBehavior.cs
+++ Source/BlazorState/Features/CloneState/Pipeline/StateTransactionBehavior.cs
@@ -87,10 +87,10 @@ public sealed class StateTransactionBehavior<TRequest, TResponse> : IPipelineBeh
       );
 
       var exceptionNotification = new ExceptionNotification
-      {
-        RequestName = nameof(StateTransactionBehavior<TRequest, TResponse>),
-        Exception = exception
-      };
+      (
+        requestName: nameof(StateTransactionBehavior<TRequest, TResponse>),
+        exception: exception
+      );
 
       await Publisher.Publish(exceptionNotification, cancellationToken);
       return default!; // It can be null, but we don't care since MediatR handles null values gracefully.
