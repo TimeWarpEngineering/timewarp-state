d01846690981d437660d81d95e5558ee1b3b5b16
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-18 13:01:40 +0700

    build sourcegen befroe runnint test also

--- RunTestApp.ps1
+++ RunTestApp.ps1
@@ -3,7 +3,8 @@ $Env:ASPNETCORE_ENVIRONMENT = "Development"
 Push-Location $PSScriptRoot
 try {
   # The analyzer is not directly referenced by the test app, so we need to build it first
-  dotnet build ./Source/TimeWarp.State.Analyzer/TimeWarp.State.Analyzer.csproj    
+  dotnet build ./Source/TimeWarp.State.Analyzer/TimeWarp.State.Analyzer.csproj
+  dotnet build ./Source/TimeWarp.State.SourceGenerator/TimeWarp.State.SourceGenerator.csproj
   dotnet run --project ./Tests/Test.App/Test.App.Server/Test.App.Server.csproj
 }
 finally {
