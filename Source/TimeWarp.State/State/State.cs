682604b96cc4428d4761bc83d2ed4bb0e35fce92
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-23 11:50:44 +0700

    State needs to implement the interface

--- Source/TimeWarp.State/State/State.cs
+++ Source/TimeWarp.State/State/State.cs
@@ -4,7 +4,7 @@ public abstract class State<TState> : IState<TState>
 {
   [IgnoreDataMember]
   public Guid Guid { get; protected init; } = Guid.NewGuid();
-  protected readonly SemaphoreSlim Semaphore = new(1, 1);
+  public SemaphoreSlim Semaphore { get; }  = new(1, 1);
 
   /// <summary>
   /// returns a new instance of type TState
