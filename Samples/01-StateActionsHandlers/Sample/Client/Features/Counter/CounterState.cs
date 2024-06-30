c4a16c4533c3371049cdf14554f1510e423d2d8b
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-30 22:41:16 +0700

    WIP Samples all need redone/updated

--- Samples/01-StateActionsHandlers/Sample/Client/Features/Counter/CounterState.cs
+++ Samples/01-StateActionsHandlers/Sample/Client/Features/Counter/CounterState.cs
@@ -1,7 +1,10 @@
 ﻿namespace Sample.Client.Features.Counter;
 
+using MediatR;
+
 public partial class CounterState : State<CounterState>
 {
   public int Count { get; private set; }
+  public CounterState(ISender sender) : base(sender) {}
   public override void Initialize() => Count = 3;
 }
