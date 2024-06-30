a74c90112ca2cf4d2387d9f750bce9b3c58d8b1b
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-30 13:46:37 +0700

    State changes WIP

--- Source/TimeWarp.State/State/IState.cs
+++ Source/TimeWarp.State/State/IState.cs
@@ -2,13 +2,18 @@ namespace TimeWarp.State;
 
 public interface IState
 {
+  ISender Sender { get; set; }
   Guid Guid { get; }
+  // string? CacheKey { get; }
+  // DateTime? TimeStamp { get; }
+  // TimeSpan CacheDuration { get; }
+  // bool IsCacheValid(string currentCacheKey);
+
   void Initialize();
 }
 
 public interface IState<out TState> : IState
 {
-
   /// <summary>
   /// Set the state from Dictionary
   /// Used by ReduxDevTools to support TimeTravel
--- Source/TimeWarp.State/State/State.cs
+++ Source/TimeWarp.State/State/State.cs
@@ -4,9 +4,41 @@ using System.Text.Json.Serialization;
 
 public abstract class State<TState> : IState<TState>
 {
+
+  #region JsonIgnore
+  // JsonIgnore is used to prevent serialization of the property by both AnyClone and ReduxDevTools 
+  
+  [JsonIgnore]
+  public ISender Sender { get; set; } = null!;
+  
+  #endregion
+
+  #region IgnoreDataMember
+
+  // IgnoreDataMember is used to prevent serialization of properties by AnyClone
+  // They change on every instance creation and are not needed for cloning
+  
   [IgnoreDataMember]
   public Guid Guid { get; protected init; } = Guid.NewGuid();
+  #endregion
   
+  public string? CacheKey { get; protected init; }
+  public DateTime? TimeStamp { get; protected init; }
+  public TimeSpan CacheDuration { get; protected init;}
+
+  /// <summary>
+  /// DI Constructor
+  /// </summary>
+  /// <param name="sender"></param>
+  protected State(ISender sender) 
+  {
+    Sender = sender;
+  }
+  
+  [JsonConstructor]
+  protected State() {}
+
+
   /// <summary>
   /// returns a new instance of type TState
   /// </summary>
@@ -29,6 +61,16 @@ public abstract class State<TState> : IState<TState>
       throw new FieldAccessException("Do not use this in production. This method is intended for Test access only!");
     }
   }
+  
+  /// <summary>
+  /// Checks if the cache is valid based on the current cache key and timestamp
+  /// </summary>
+  /// <param name="currentCacheKey">The cache key to validate against</param>
+  /// <returns>True if the cache is valid, otherwise false</returns>
+  public bool IsCacheValid(string currentCacheKey) => 
+    CacheKey == currentCacheKey &&
+    TimeStamp.HasValue &&
+    (DateTime.UtcNow - TimeStamp.Value) < CacheDuration;
 
   /// <summary>
   /// Override this to Set the initial state
