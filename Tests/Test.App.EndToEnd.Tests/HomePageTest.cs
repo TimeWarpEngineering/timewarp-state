2e79aab686654b1c6d53ace189fee86b6c1cffd5
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-07-02 19:07:49 +0700

    Ensure page loads has title and proper render modes

--- Tests/Test.App.EndToEnd.Tests/HomePageTest.cs
+++ Tests/Test.App.EndToEnd.Tests/HomePageTest.cs
@@ -1,5 +1,6 @@
 namespace HomePage_;
 
+using FluentAssertions;
 using Test.App.EndToEnd.Tests;
 
 [TestClass]
@@ -8,8 +9,18 @@ public class Should_ : PageTest
   [TestMethod]
   public async Task HaveTitle()
   {
-    await Page.GotoAsync(Configuration.SutBaseHttpsUrl);
+    string sutBaseUrl = Configuration.GetSutBaseUrl();
+    Console.WriteLine($"Sut Base Url: {sutBaseUrl}");
+    await Page.GotoAsync(sutBaseUrl);
     
     await Expect(Page).ToHaveTitleAsync("TimeWarp.State Test App");
+   
+    // Validate Current Render Mode
+    string? currentRenderModeText = await Page.Locator("text=Current Render Mode: Static").TextContentAsync();
+    currentRenderModeText.Should().Be("Current Render Mode: Static");
+
+    // Validate Configured Render Mode
+    string? configuredRenderModeText = await Page.Locator("text=Configured Render Mode: None").TextContentAsync();
+    configuredRenderModeText.Should().Be("Configured Render Mode: None");
   }
 }
