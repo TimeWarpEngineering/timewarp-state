07964bf19c94955143447f50669a62baec96aa83
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-07-02 18:26:58 +0700

    Use environment variable to determine SUT Port.

--- Tests/Test.App.EndToEnd.Tests/Configuration.cs
+++ Tests/Test.App.EndToEnd.Tests/Configuration.cs
@@ -2,5 +2,10 @@ namespace Test.App.EndToEnd.Tests;
 
 public static class Configuration
 {
-  public const string SutBaseHttpsUrl = "https://localhost:7011/";
+  public static string GetSutBaseUrl()
+  {
+    string port = Environment.GetEnvironmentVariable("SutPort") ?? "7011";
+    string sutBaseHttpsUrl = $"https://localhost:{port}/";
+    return sutBaseHttpsUrl;
+  }
 }
