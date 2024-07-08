907836eb7ea9e014ee61466cbcfdbea31c9e477c
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-07-08 20:54:06 +0700

    try to build the test project before running

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,6 +1,7 @@
 Push-Location $PSScriptRoot
 try {
     dotnet restore
+    dotnet build --project ./Tests/TimeWarp.State.Analyzer.Tests/TimeWarp.State.Analyzer.Tests.csproj
     dotnet fixie TimeWarp.State.Analyzer.Tests TimeWarp.State.Tests Client.Integration.Tests TimeWarp.State.Plus.Tests
 }
 finally {
