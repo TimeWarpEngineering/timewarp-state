f8de6a1d95e24673adcf6b854090d67e9292e1bb
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-07-18 01:44:55 +0700

    List LocalNugetFeed before running tests.

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,6 +1,7 @@
 Push-Location $PSScriptRoot
 try {
-    New-Item -ItemType Directory -Force -Path./LocalNugetFeed
+    # List files in LocalNugetFeed
+    Get-ChildItem ./LocalNugetFeed
     dotnet restore
     dotnet build --project ./Tests/TimeWarp.State.Analyzer.Tests/TimeWarp.State.Analyzer.Tests.csproj
     dotnet fixie TimeWarp.State.Analyzer.Tests TimeWarp.State.Tests Client.Integration.Tests TimeWarp.State.Plus.Tests
