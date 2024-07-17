d197be4524a0acc24e8e71ef6b146cbd24dcea08
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-07-18 01:21:41 +0700

    Use ProjectDir instead of SolutionDir because Solution dir isn't defined from cli when building just a project.
    and create the LocalNugetFeed directly from the RutTests.ps1 script.

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,5 +1,6 @@
 Push-Location $PSScriptRoot
 try {
+    New-Item -ItemType Directory -Force -Path./LocalNugetFeed
     dotnet restore
     dotnet build --project ./Tests/TimeWarp.State.Analyzer.Tests/TimeWarp.State.Analyzer.Tests.csproj
     dotnet fixie TimeWarp.State.Analyzer.Tests TimeWarp.State.Tests Client.Integration.Tests TimeWarp.State.Plus.Tests
