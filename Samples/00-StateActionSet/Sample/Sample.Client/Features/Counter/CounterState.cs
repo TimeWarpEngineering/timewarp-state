69a198edf049d1d2b01cff0dd574d532854f6fdd
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-07-21 17:57:43 +0700

    Update Sample 00

--- Samples/00-StateActionSet/Sample/Sample.Client/Features/Counter/CounterState.cs
+++ Samples/00-StateActionSet/Sample/Sample.Client/Features/Counter/CounterState.cs
@@ -3,8 +3,11 @@ namespace Sample.Client.Features.Counter;
 
 using TimeWarp.State;
 
-public partial class CounterState : State<CounterState>
+internal sealed partial class CounterState : State<CounterState>
 {
   public int Count { get; private set; }
-  public override void Initialize() => Count = 3;
+  public override void Initialize()
+  {
+    Count = 3;
+  }
 }
