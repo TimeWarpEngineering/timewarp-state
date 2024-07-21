2a5ecea82a6f9086bf945a192978132c344976c2
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-07-21 20:25:15 +0700

    Improved the BuildNuGets.ps1 script by adding error handling, creating the LocalNugetFeed directory if it doesn't exist, and simplifying the build process using a foreach loop.

--- BuildNuGets.ps1
+++ BuildNuGets.ps1
@@ -3,20 +3,24 @@ try
 {
   $Configuration = "Release"
   dotnet cleanup -y
-  Remove-Item "./LocalNugetFeed" -Recurse -Force
-  Remove-Item "./Source/TimeWarp.State/wwwroot\js" -Recurse -Force
-  Push-Location ./Source/TimeWarp.State.Analyzer/
-  dotnet build --configuration $Configuration
-  Pop-Location
-  Push-Location ./Source/TimeWarp.State.SourceGenerator/
-  dotnet build --configuration $Configuration
-  Pop-Location
-  Push-Location ./Source/TimeWarp.State/
-  dotnet build --configuration $Configuration
-  Pop-Location
-  Push-Location ./Source/TimeWarp.State.Plus/
-  dotnet build --configuration $Configuration
-  Pop-Location
+  Remove-Item "./LocalNugetFeed" -Recurse -Force -ErrorAction SilentlyContinue
+  Remove-Item "./Source/TimeWarp.State/wwwroot\js" -Recurse -Force -ErrorAction SilentlyContinue
+  New-Item -ItemType Directory -Force -Path "./LocalNugetFeed"
+
+  $projects = @(
+    "./Source/TimeWarp.State.Analyzer/",
+    "./Source/TimeWarp.State.SourceGenerator/",
+    "./Source/TimeWarp.State/",
+    "./Source/TimeWarp.State.Plus/"
+  )
+
+  foreach ($project in $projects) {
+    Push-Location $project
+    dotnet build --configuration $Configuration
+    Pop-Location
+  }
+
+  Write-Host "Projects have been built successfully"
 }
 finally
 {
