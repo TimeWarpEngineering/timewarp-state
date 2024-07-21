24c3c939a1d9ddfcab398ebc2a8f41d11f4a9b08
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-07-21 17:58:19 +0700

    Use a Configuration variable

--- BuildNuGets.ps1
+++ BuildNuGets.ps1
@@ -1,25 +1,24 @@
 Push-Location $PSScriptRoot
-try {
-    dotnet cleanup -y
-    Push-Location ./Source/TimeWarp.State.Analyzer/
-    # dotnet clean --configuration Release
-    dotnet build --configuration Release
-    Pop-Location
-    Push-Location ./Source/TimeWarp.State.SourceGenerator/
-    # dotnet clean --configuration Release
-    dotnet build --configuration Release
-    Pop-Location
-    Push-Location ./Source/TimeWarp.State/
-    # dotnet clean --configuration Release
-    dotnet build --configuration Release
-    # dotnet pack --configuration Release
-    Pop-Location
-    Push-Location ./Source/TimeWarp.State.Plus/
-    # dotnet clean --configuration Release
-    dotnet build --configuration Release
-    # dotnet pack --configuration Release
-    Pop-Location
+try
+{
+  $Configuration = "Release"
+  dotnet cleanup -y
+  Remove-Item "./LocalNugetFeed" -Recurse -Force
+  Remove-Item "./Source/TimeWarp.State/wwwroot\js" -Recurse -Force
+  Push-Location ./Source/TimeWarp.State.Analyzer/
+  dotnet build --configuration $Configuration
+  Pop-Location
+  Push-Location ./Source/TimeWarp.State.SourceGenerator/
+  dotnet build --configuration $Configuration
+  Pop-Location
+  Push-Location ./Source/TimeWarp.State/
+  dotnet build --configuration $Configuration
+  Pop-Location
+  Push-Location ./Source/TimeWarp.State.Plus/
+  dotnet build --configuration $Configuration
+  Pop-Location
 }
-finally {
+finally
+{
   Pop-Location
 }
