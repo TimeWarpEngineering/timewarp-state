f44f117928be5255af7b21148313567137ee30de
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-07-31 21:51:05 +0700

    New generated code

--- Tests/Test.App/Test.App.Client/Generated/TimeWarp.State.SourceGenerator/TimeWarp.State.SourceGenerator.PersistenceStateSourceGenerator/Test.App.Client.Features.Blue.BlueState_Persistence.g.cs
+++ Tests/Test.App/Test.App.Client/Generated/TimeWarp.State.SourceGenerator/TimeWarp.State.SourceGenerator.PersistenceStateSourceGenerator/Test.App.Client.Features.Blue.BlueState_Persistence.g.cs
@@ -8,29 +8,40 @@ using TimeWarp.State;
 
 public partial class BlueState
 {
-    public static class Load
-    {
-        public class Action : IAction { }
+  public static class Load
+  {
+    public class Action : IAction { }
 
-        public class Handler
-        (
-          IStore store,
-          IPersistenceService PersistenceService
-        ): ActionHandler<Action>(store)
+    public class Handler : ActionHandler<Action>
+    {
+      private readonly IPersistenceService PersistenceService;
+      private readonly ILogger<Handler> Logger;
+      
+      public Handler
+      (
+        IStore store,
+        IPersistenceService persistenceService,
+        ILogger<Handler> logger
+      ) : base(store)
+      {
+        PersistenceService = persistenceService;
+        Logger = logger;
+      }
+      public override async System.Threading.Tasks.Task Handle(Action action, System.Threading.CancellationToken cancellationToken)
+      {
+        try
         {
-            public override async System.Threading.Tasks.Task Handle(Action action, System.Threading.CancellationToken cancellationToken)
-            {
-                try
-                {
-                    object? state = await PersistenceService.LoadState(typeof(BlueState), PersistentStateMethod.SessionStorage);
-                    if (state is BlueState blueState) Store.SetState(blueState);
-                }
-                catch (Exception)
-                {
-                    // if this is a JavaScript not available exception then we are prerendering and just swallow it
-                }
-            }
+            object? state = await PersistenceService.LoadState(typeof(BlueState), PersistentStateMethod.SessionStorage);
+            if (state is BlueState blueState) Store.SetState(blueState);
+            else Logger.LogTrace("BlueState is null");
         }
+        catch (Exception exception)
+        {
+          Logger.LogError(exception, "Error loading {{className}}");
+          // if this is a JavaScript not available exception then we are prerendering and just swallow it
+        }
+      }
     }
+  }
 }
 #pragma warning restore CS1591
--- Tests/Test.App/Test.App.Client/Generated/TimeWarp.State.SourceGenerator/TimeWarp.State.SourceGenerator.PersistenceStateSourceGenerator/Test.App.Client.Features.Purple.PurpleState_Persistence.g.cs
+++ Tests/Test.App/Test.App.Client/Generated/TimeWarp.State.SourceGenerator/TimeWarp.State.SourceGenerator.PersistenceStateSourceGenerator/Test.App.Client.Features.Purple.PurpleState_Persistence.g.cs
@@ -8,29 +8,40 @@ using TimeWarp.State;
 
 public partial class PurpleState
 {
-    public static class Load
-    {
-        public class Action : IAction { }
+  public static class Load
+  {
+    public class Action : IAction { }
 
-        public class Handler
-        (
-          IStore store,
-          IPersistenceService PersistenceService
-        ): ActionHandler<Action>(store)
+    public class Handler : ActionHandler<Action>
+    {
+      private readonly IPersistenceService PersistenceService;
+      private readonly ILogger<Handler> Logger;
+      
+      public Handler
+      (
+        IStore store,
+        IPersistenceService persistenceService,
+        ILogger<Handler> logger
+      ) : base(store)
+      {
+        PersistenceService = persistenceService;
+        Logger = logger;
+      }
+      public override async System.Threading.Tasks.Task Handle(Action action, System.Threading.CancellationToken cancellationToken)
+      {
+        try
         {
-            public override async System.Threading.Tasks.Task Handle(Action action, System.Threading.CancellationToken cancellationToken)
-            {
-                try
-                {
-                    object? state = await PersistenceService.LoadState(typeof(PurpleState), PersistentStateMethod.LocalStorage);
-                    if (state is PurpleState purpleState) Store.SetState(purpleState);
-                }
-                catch (Exception)
-                {
-                    // if this is a JavaScript not available exception then we are prerendering and just swallow it
-                }
-            }
+            object? state = await PersistenceService.LoadState(typeof(PurpleState), PersistentStateMethod.LocalStorage);
+            if (state is PurpleState purpleState) Store.SetState(purpleState);
+            else Logger.LogTrace("PurpleState is null");
         }
+        catch (Exception exception)
+        {
+          Logger.LogError(exception, "Error loading {{className}}");
+          // if this is a JavaScript not available exception then we are prerendering and just swallow it
+        }
+      }
     }
+  }
 }
 #pragma warning restore CS1591
