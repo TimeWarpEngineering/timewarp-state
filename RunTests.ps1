0b6d4393c03ed241303532a7e3af3c3b07d51381
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-08-05 13:56:15 +0700

    Fix Test name

--- RunTests.ps1
+++ RunTests.ps1
@@ -4,7 +4,7 @@ try {
     Get-ChildItem ./LocalNugetFeed
     dotnet restore
     dotnet build --project ./Tests/TimeWarp.State.Analyzer.Tests/TimeWarp.State.Analyzer.Tests.csproj
-    dotnet fixie TimeWarp.State.Analyzer.Tests TimeWarp.State.Tests TimeWarp.State.Plus.Tests TimeWarp.Archtecture.Tests Client.Integration.Tests 
+    dotnet fixie TimeWarp.State.Analyzer.Tests TimeWarp.State.Tests TimeWarp.State.Plus.Tests Client.Integration.Tests Test.App.Architecture.Tests
 }
 finally {
     Pop-Location
