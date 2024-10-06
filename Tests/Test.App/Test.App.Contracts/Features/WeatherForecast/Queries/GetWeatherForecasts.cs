39b6eb7c7f798c5f6843381392a558cd14c6b12a
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-10-07 05:35:44 +0700

    fix: update weather forecast query route template

--- Tests/Test.App/Test.App.Contracts/Features/WeatherForecast/Queries/GetWeatherForecasts.cs
+++ Tests/Test.App/Test.App.Contracts/Features/WeatherForecast/Queries/GetWeatherForecasts.cs
@@ -5,13 +5,13 @@ public static class GetWeatherForecasts
   public sealed class Query : IRequest<Response>
   {
     public int Days { get; init; }
-    
-    public const string RouteTemplate = "api/weatherForecasts";
-    public string GetRoute() => FormattableString.Invariant($"api/weather");
+
+    public const string RouteTemplate = "api/weather";
+    public string GetRoute() => FormattableString.Invariant($"{RouteTemplate}");
   }
-  
-  public sealed class Response : List<WeatherForecastDto> { }
-  
+
+  public sealed class Response : List<WeatherForecastDto>;
+
   public sealed class WeatherForecastDto
   {
     public DateOnly Date { get; }
