ff374cd8d1024245e1aca3954672a98d21c5b707
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-11-24 14:23:26 +0700

    Add CounterState IncrementCount implementation
    - Add IncrementCountActionSet with Action and Handler
    - Implement Handler to mutate CounterState
    - Add IncrementCount method to send Action through pipeline

---
+++ Samples/00-StateActionHandler/Sample00/Sample00.Client/Features/Counter/CounterState.IncrementCount.cs
@@ -0,0 +1,31 @@
+namespace Sample00.Client.Features.Counter;
+
+internal partial class CounterState
+{
+    public static class IncrementCountActionSet
+    {
+        public sealed class Action : IAction
+        {
+            public int Amount { get; }
+            public Action(int amount)
+            {
+                Amount = amount;
+            }
+        }
+        
+        public sealed class Handler : ActionHandler<Action>
+        {
+            public Handler(IStore store) : base(store) { }
+            
+            private CounterState CounterState => Store.GetState<CounterState>();
+            public override Task Handle(Action action, CancellationToken cancellationToken)
+            {
+                CounterState.Count += action.Amount;
+                return Task.CompletedTask;
+            }
+        }
+    }
+    
+    public async Task IncrementCount(int amount = 1, CancellationToken cancellationToken = default) =>
+        await Sender.Send(new IncrementCountActionSet.Action(amount), cancellationToken);
+}
