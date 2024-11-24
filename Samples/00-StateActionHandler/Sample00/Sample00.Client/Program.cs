77f1b3c4e2b9a41d6c2297f990660ffa5b57d547
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-11-24 11:08:03 +0700

    Add Sample00 initial Blazor project
    - Create new Blazor project with Interactive Auto mode
    - Include Client and Server projects
    - Set up basic project structure and files
    - Configure for program-main usage

---
+++ Samples/00-StateActionHandler/Sample00/Sample00.Client/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Sample00.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        await builder.Build().RunAsync();
+    }
+}
---
+++ Samples/00-StateActionHandler/Sample00/Sample00/Program.cs
@@ -0,0 +1,43 @@
+using Sample00.Client.Pages;
+using Sample00.Components;
+
+namespace Sample00;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+        app.Run();
+    }
+}
