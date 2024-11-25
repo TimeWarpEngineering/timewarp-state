32a7ccb06afb1f819c925ec8b215f47408c87f98
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-11-25 19:04:33 +0700

    fix: Resolve build issues in WebAssembly sample
    - Fix ambiguous Program reference in Routes.razor
    - Configure WebAssembly components in Server Program.cs
    - Add version bounds to package references to resolve warnings

--- Samples/00-StateActionHandler/Wasm/Sample00Wasm/Sample00Wasm/Program.cs
+++ Samples/00-StateActionHandler/Wasm/Sample00Wasm/Sample00Wasm/Program.cs
@@ -1,6 +1,3 @@
-using Sample00Wasm.Client.Pages;
-using Sample00Wasm.Components;
-
 namespace Sample00Wasm;
 
 public class Program
@@ -23,18 +20,16 @@ public class Program
         else
         {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
         app.UseHttpsRedirection();
-
         app.UseStaticFiles();
         app.UseAntiforgery();
 
         app.MapRazorComponents<App>()
             .AddInteractiveWebAssemblyRenderMode()
-            .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+            .AddAdditionalAssemblies(typeof(Sample00Wasm.Client._Imports).Assembly);
 
         app.Run();
     }
