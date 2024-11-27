f7b64bd29fe1a3e075c0414577390cf0487e17e6
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-11-27 14:15:38 +0700

    Task 006: Fix namespace references in GlobalUsings.cs and Routes.razor

--- Samples/00-StateActionHandler/Auto/Sample00Auto/Sample00Auto/GlobalUsings.cs
+++ Samples/00-StateActionHandler/Auto/Sample00Auto/Sample00Auto/GlobalUsings.cs
@@ -2,6 +2,6 @@ global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.Extensions.DependencyInjection;
-global using Sample00.Client.Pages;
-global using Sample00.Components;
+global using Sample00Auto.Client.Pages;
+global using Sample00Auto.Components;
 global using TimeWarp.State;
