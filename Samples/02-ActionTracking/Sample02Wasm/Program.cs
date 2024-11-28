5c89e74913c225e31eeaff0bf3437165e41f1c74
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-11-28 23:26:17 +0700

    Remove redundant AddTimeWarpStatePlus call

--- Samples/02-ActionTracking/Sample02Wasm/Program.cs
+++ Samples/02-ActionTracking/Sample02Wasm/Program.cs
@@ -11,7 +11,6 @@ public class Program
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
         
         builder.Services.AddTimeWarpState();
-        builder.Services.AddTimeWarpStatePlus(); // Adds Action Tracking and other Plus features
 
         await builder.Build().RunAsync();
     }
