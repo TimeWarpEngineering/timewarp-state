721ac885470ea9dd97f8f88e307b841f6754a4ca
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-11-29 07:19:33 +0700

    Register TimeWarp.State.Plus assembly in AddTimeWarpState options

--- Samples/02-ActionTracking/Sample02Wasm/Program.cs
+++ Samples/02-ActionTracking/Sample02Wasm/Program.cs
@@ -10,7 +10,17 @@ public class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
         
-        builder.Services.AddTimeWarpState();
+        builder.Services.AddTimeWarpState
+        (
+            options =>
+            {
+                options.Assemblies = new[]
+                {
+                    typeof(Program).Assembly,
+                    typeof(TimeWarp.State.Plus.AssemblyMarker).Assembly
+                };
+            }
+        );
 
         await builder.Build().RunAsync();
     }
