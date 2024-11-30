d15974d2343f4e6d612b38fd07301fd1acc76e53
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-11-30 07:51:54 +0700

    fix: Add TimeWarp.State.Plus using directive

--- Samples/03-Routing/Wasm/Sample03Wasm/GlobalUsings.cs
+++ Samples/03-Routing/Wasm/Sample03Wasm/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 global using TimeWarp.State;
 global using TimeWarp.Features.Routing;
+global using TimeWarp.State.Plus;
