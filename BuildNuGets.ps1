cfcc54510837577563d9187ca98b9cd0c93b842d
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-12-05 13:31:44 +0700

    SilentlyContinue if no dotnet process

--- BuildNuGets.ps1
+++ BuildNuGets.ps1
@@ -1,7 +1,7 @@
 Push-Location $PSScriptRoot
 try
 {
-  Get-Process dotnet | Stop-Process
+  Get-Process dotnet | Stop-Process -ErrorAction SilentlyContinue
   dotnet nuget locals all --clear
   $Configuration = "Release"
   dotnet tool restore
